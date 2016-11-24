9cb63a793d180cca90939289e9377110363c01fd
bdanglot <bdanglot@gmail.com>
2016-11-24 08:57:33 +0100

    setup travis ci

---
+++ .travis_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ..
+
+git clone https://github.com/DIVERSIFY-project/sosiefier.git
+cd sosiefier
+mvn install
+
+git clone https://github.com/DIVERSIFY-project/profiling.git
+
+cd ../dspot
