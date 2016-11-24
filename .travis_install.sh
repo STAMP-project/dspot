c2b50f0514329fced4d2921ca826e91b94c96be0
bdanglot <bdanglot@gmail.com>
2016-11-24 11:47:37 +0100

    add travis' badge

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,8 +6,10 @@ git clone https://github.com/DIVERSIFY-project/sosiefier.git
 cd sosiefier
 mvn install
 
-git clone https://github.com/DIVERSIFY-project/profiling.git
-cd profiling
+cd ..
+
+git clone https://github.com/DIVERSIFY-project/diversify-profiling.git
+cd diversify-profiling
 mvn install
 
 cd ../dspot
