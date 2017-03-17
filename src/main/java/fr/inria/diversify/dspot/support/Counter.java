ee6272888fa6bd7ee55a8ff4c0eb54075aa4fa86
bdanglot <bdanglot@gmail.com>
2017-03-17 09:10:49 +0100

    fix Counter

--- src/main/java/fr/inria/diversify/dspot/support/Counter.java
+++ src/main/java/fr/inria/diversify/dspot/support/Counter.java
@@ -15,9 +15,11 @@ public class Counter {
 
     private static Counter _instance;
 
-    private Map<CtMethod, Integer> numberOfAssertionAdded;
+    //private Map<CtMethod<?>, Integer> numberOfAssertionAdded;
+    private Map<String, Integer> numberOfAssertionAdded;
 
-    private Map<CtMethod, Integer> numberOfInputAdded;
+    //private Map<CtMethod<?>, Integer> numberOfInputAdded;
+    private Map<String, Integer> numberOfInputAdded;
 
     private Counter() {
         this.numberOfAssertionAdded = new HashMap<>();
@@ -31,25 +33,25 @@ public class Counter {
         return _instance;
     }
 
-    public static void updateAssertionOf(CtMethod method, int number) {
+    public static void updateAssertionOf(CtMethod<?> method, int number) {
         updateGivenMap(method, number, getInstance().numberOfAssertionAdded);
     }
 
-    public static void updateInputOf(CtMethod method, int number) {
+    public static void updateInputOf(CtMethod<?> method, int number) {
         updateGivenMap(method, number, getInstance().numberOfInputAdded);
     }
 
-    public static Integer getAssertionOf(CtMethod method) {
-        return getInstance().numberOfAssertionAdded.get(method) == null ? 0 : getInstance().numberOfAssertionAdded.get(method);
+    public static Integer getAssertionOf(CtMethod<?> method) {
+        return getInstance().numberOfAssertionAdded.get(method.getSimpleName()) == null ? 0 : getInstance().numberOfAssertionAdded.get(method.getSimpleName());
     }
 
-    public static Integer getInputOf(CtMethod method) {
-        return getInstance().numberOfInputAdded.get(method) == null ? 0 : getInstance().numberOfInputAdded.get(method);
+    public static Integer getInputOf(CtMethod<?> method) {
+        return getInstance().numberOfInputAdded.get(method.getSimpleName()) == null ? 0 : getInstance().numberOfInputAdded.get(method.getSimpleName());
     }
 
-    public static Integer getAssertionOfSinceOrigin(CtMethod method) {
-        CtMethod currentMethod = method;
-        CtMethod parent;
+    public static Integer getAssertionOfSinceOrigin(CtMethod<?> method) {
+        CtMethod<?> currentMethod = method;
+        CtMethod<?> parent;
         int countAssertion = getAssertionOf(currentMethod);
         while ((parent = AmplificationHelper.getAmpTestToParent().get(currentMethod)) != null) {
             currentMethod = parent;
@@ -58,9 +60,9 @@ public class Counter {
         return countAssertion;
     }
 
-    public static Integer getInputOfSinceOrigin(CtMethod method) {
-        CtMethod currentMethod = method;
-        CtMethod parent;
+    public static Integer getInputOfSinceOrigin(CtMethod<?> method) {
+        CtMethod<?> currentMethod = method;
+        CtMethod<?> parent;
         int countAssertion = 0;
         while ((parent = AmplificationHelper.getAmpTestToParent().get(currentMethod)) != null) {
             countAssertion += getInputOf(currentMethod);
@@ -70,11 +72,11 @@ public class Counter {
         return countAssertion;
     }
 
-    private static void updateGivenMap(CtMethod method, int number, Map<CtMethod, Integer> mapToBeUpdated) {
-        if (!mapToBeUpdated.containsKey(method)) {
-            mapToBeUpdated.put(method, 0);
+    private static void updateGivenMap(CtMethod<?> method, int number, Map<String, Integer> mapToBeUpdated) {
+        if (!mapToBeUpdated.containsKey(method.getSimpleName())) {
+            mapToBeUpdated.put(method.getSimpleName(), 0);
         }
-        mapToBeUpdated.put(method, mapToBeUpdated.get(method) + number);
+        mapToBeUpdated.put(method.getSimpleName(), mapToBeUpdated.get(method.getSimpleName()) + number);
     }
 
 }
