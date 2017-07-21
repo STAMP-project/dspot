1e3fff86708df0e845c2ff1a346a1cd6fd30c0ed
Daniele Gagliardi <daniele.gagliardi@eng.it>
2017-07-21 17:17:32 +0200

    #96: implemented runPit() and compile() methods. Implementation complete. Missing support to Descartes mode, due to pitest version loaded from classpath: need for investigation

--- src/test/java/fr/inria/diversify/automaticbuilder/GradleAutomaticBuilderTest.java
+++ src/test/java/fr/inria/diversify/automaticbuilder/GradleAutomaticBuilderTest.java
@@ -1,14 +1,21 @@
 package fr.inria.diversify.automaticbuilder;
 
+import fr.inria.diversify.Utils;
 import fr.inria.diversify.dspot.DSpotUtils;
+import fr.inria.diversify.mutant.pit.PitResult;
 import fr.inria.diversify.runner.InputConfiguration;
+import fr.inria.diversify.util.FileUtils;
+import fr.inria.diversify.util.Log;
 import fr.inria.stamp.Configuration;
 import fr.inria.stamp.JSAPOptions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import spoon.reflect.declaration.CtClass;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -29,23 +36,45 @@ public class GradleAutomaticBuilderTest {
 
     @Before
     public void setUp() throws Exception {
+
+        cleanTestEnv();
+
+        Utils.init("src/test/resources/sample/sample.properties");
+
+        Log.debug("Test Set-up - Reading input parameters...");
         this.configuration = JSAPOptions.parse(getArgsWithGradleBuilder());
         InputConfiguration inputConfiguration = new InputConfiguration(configuration.pathToConfigurationFile);
         inputConfiguration.getProperties().setProperty("automaticBuilderName", configuration.automaticBuilderName);
 
+        Log.debug("Test Set-up - instantiating Automatic Builder (SUT)...");
         sut = factory.getAutomaticBuilder(inputConfiguration);
+        Log.debug("Test Set-up complete.");
     }
 
     @After
     public void tearDown() throws Exception {
+        Log.debug("Test Tear-down...");
+
+        cleanTestEnv();
+
+        Log.debug("Test Tear-down complete.");
     }
 
     @Test
-    public void compile() throws Exception {
+    public void compile_whenCleanCompileTestTasksAreAppliedToDSpotTestExampleProject() throws Exception {
+        Log.info("Starting Gradle Automatic Builder compile() test...");
+        sut.compile("src/test/resources/test-projects/");
+
+        File buildDir = new File("src/test/resources/test-projects/build");
+        assertTrue("After compilation, build dir should exist", buildDir.exists());
+
+        Log.info("Gradle Automatic Builder compile() test complete.");
     }
 
     @Test
-    public void buildClasspath() throws Exception {
+    public void buildClasspath_whenAppliedToDSpotTestExampleProject() throws Exception {
+        Log.info("Starting Gradle Automatic Builder buildClasspath() test...");
+
         String[] expectedCompilationLibraries = {"log4j-api-2.8.2.jar", "log4j-core-2.8.2.jar"};
         String[] expectedTestCompilationLibraries = {"hamcrest-core-1.3.jar", "junit-4.12.jar"};
 
@@ -58,16 +87,34 @@ public class GradleAutomaticBuilderTest {
 
         assertTrue("Classpath should contain " + expectedTestCompilationLibraries[0] + " library as test dependency", classPath.contains(expectedTestCompilationLibraries[0])); // test compile dependency
         assertTrue("Classpath should contain " + expectedTestCompilationLibraries[1] + " library as test dependency", classPath.contains(expectedTestCompilationLibraries[1])); // test compile dependency
+
+        Log.info("Gradle Automatic Builder buildClasspath() test complete.");
     }
 
     @Test
-    public void runPit() throws Exception {
-        sut.runPit("src/test/resources/test-projects/");
-        assertTrue(true);
+    public void runPit_whenNoTestClassIsSpecified() throws Exception {
+        Log.info("Starting Gradle Automatic Builder runPit() test when no test class is specified...");
+
+        List<PitResult> pitResults = sut.runPit("src/test/resources/test-projects/");
+
+        assertTrue("PIT results shouldn't be null", pitResults != null);
+        assertTrue("PIT results shouldn't be empty", !pitResults.isEmpty());
+
+        Log.info("Gradle Automatic Builder runPit() test complete when no test class is specified.");
     }
 
     @Test
-    public void runPit1() throws Exception {
+    public void runPit_whenTestClassIsSpecified() throws Exception {
+        Log.info("Starting Gradle Automatic Builder runPit() test when a test class is specified...");
+
+        CtClass<Object> testClass = Utils.getInputProgram().getFactory().Class().get("example.TestSuiteExample");
+
+        List<PitResult> pitResults = sut.runPit("src/test/resources/test-projects/", testClass);
+
+        assertTrue("PIT results shouldn't be null", pitResults != null);
+        assertTrue("PIT results shouldn't be empty", !pitResults.isEmpty());
+
+        Log.info("Gradle Automatic Builder runPit() test complete when a test class is specified.");
     }
 
     private String[] getArgsWithGradleBuilder() throws IOException {
@@ -80,6 +127,21 @@ public class GradleAutomaticBuilderTest {
                 "--automatic-builder", "GradleBuilder",
                 "--test", "all"
         };
+    }
+
+    private void cleanTestEnv() throws IOException {
+        File classPathFile = new File("src/test/resources/test-projects/gjp_cp");
+        if (classPathFile.exists()) {
+            Log.debug("Cleaning Test Env - Deleting java classpath file...");
+            classPathFile.delete();
+        }
+
+        File buildDir = new File("src/test/resources/test-projects/build");
+        if (buildDir.exists()) {
+            Log.debug("Cleaning Test Env - Deleting Gradle Java project build dir...");
+            FileUtils.deleteDirectory(buildDir);
+        }
+        Log.debug("Test Env cleaning complete.");
     }
 
 }
