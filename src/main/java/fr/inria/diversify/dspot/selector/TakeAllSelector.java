6b412da3f50a4f82cb79ee086e5e2b57107aaf6e
danglotb <bdanglot@gmail.com>
2017-08-08 13:27:33 +0200

    add TakeAllSelector

---
+++ src/main/java/fr/inria/diversify/dspot/selector/TakeAllSelector.java
@@ -0,0 +1,74 @@
+package fr.inria.diversify.dspot.selector;
+
+import fr.inria.diversify.runner.InputConfiguration;
+import fr.inria.diversify.runner.InputProgram;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 08/08/17
+ */
+public class TakeAllSelector implements TestSelector {
+
+	protected List<CtMethod> selectedAmplifiedTest;
+
+	protected InputConfiguration configuration;
+
+	protected InputProgram program;
+
+	protected CtType<?> currentClassTestToBeAmplified;
+
+	public TakeAllSelector() {
+		this.selectedAmplifiedTest = new ArrayList<>();
+	}
+
+	@Override
+	public void init(InputConfiguration configuration) {
+		this.configuration = configuration;
+		this.program = this.configuration.getInputProgram();
+	}
+
+	@Override
+	public void reset() {
+		//empty
+	}
+
+	@Override
+	public List<CtMethod<?>> selectToAmplify(List<CtMethod<?>> testsToBeAmplified) {
+		return testsToBeAmplified;
+	}
+
+	@Override
+	public List<CtMethod<?>> selectToKeep(List<CtMethod<?>> amplifiedTestToBeKept) {
+		this.selectedAmplifiedTest.addAll(amplifiedTestToBeKept);
+		return amplifiedTestToBeKept;
+	}
+
+	@Override
+	public void update() {
+		//empty
+	}
+
+	@Override
+	public void report() {
+		System.out.println("Ampification results with " + this.selectedAmplifiedTest.size() + " new tests.");
+	}
+
+	@Override
+	public int getNbAmplifiedTestCase() {
+		return this.selectedAmplifiedTest.size();
+	}
+
+	@Override
+	public CtType buildClassForSelection(CtType original, List<CtMethod<?>> methods) {
+		CtType clone = original.clone();
+		original.getPackage().addType(clone);
+		methods.forEach(clone::addMethod);
+		return clone;
+	}
+}
