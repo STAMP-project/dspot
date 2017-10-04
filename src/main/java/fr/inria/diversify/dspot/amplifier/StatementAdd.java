d58a450e9a8128e42e92dd30d10bc05d2a189e73
danglotb <bdanglot@gmail.com>
2017-10-04 13:50:28 +0200

    statement add now works properly

--- src/main/java/fr/inria/diversify/dspot/amplifier/StatementAdd.java
+++ src/main/java/fr/inria/diversify/dspot/amplifier/StatementAdd.java
@@ -5,6 +5,8 @@ import fr.inria.diversify.dspot.amplifier.value.ValueCreator;
 import fr.inria.diversify.utils.AmplificationHelper;
 import fr.inria.diversify.utils.DSpotUtils;
 import fr.inria.diversify.utils.TypeUtils;
+import org.jacoco.core.data.ExecutionDataStore;
+import org.kevoree.log.Log;
 import spoon.reflect.code.*;
 import spoon.reflect.declaration.*;
 import spoon.reflect.factory.Factory;
@@ -60,33 +62,59 @@ public class StatementAdd implements Amplifier {
 	}
 
 	private List<CtMethod> useReturnValuesOfExistingMethodCall(CtMethod method) {
-		final int[] count = new int[1];
-		count[0] = 0;
 		List<CtInvocation> invocations = getInvocation(method);
 		final List<CtMethod> ampMethods = new ArrayList<>();
 		invocations.stream()
-				.filter(invocation -> !TypeUtils.isPrimitive(invocation.getType()) || !TypeUtils.isString(invocation.getType()))
+				.filter(invocation -> !TypeUtils.isPrimitive(invocation.getType()) ||
+						!TypeUtils.isString(invocation.getType()))
 				.forEach(invocation -> {
 					List<CtMethod> methodsWithTargetType = findMethodsWithTargetType(invocation.getType());
 					if (!methodsWithTargetType.isEmpty()) {
+						int indexOfInvocation = getIndexOf(method, invocation);
 						CtLocalVariable localVar = method.getFactory().Code().createLocalVariable(
 								invocation.getType(),
-								"__DSPOT_invoc_" + count[0]++,
-								invocation);
-
+								"__DSPOT_invoc_" + indexOfInvocation,
+								invocation.clone());
 						CtExpression<?> target = createLocalVarRef(localVar);
 						CtMethod methodClone = AmplificationHelper.cloneMethodTest(method, "");
-						CtStatement stmt = findInvocationIn(methodClone, invocation);
-						stmt.replace(localVar);
-
+						replaceInvocationByLocalVariable(
+								methodClone.getElements(new TypeFilter<>(CtStatement.class)).get(indexOfInvocation),
+								localVar
+						);
 						DSpotUtils.addComment(localVar, "StatementAdd: generate variable from return value", CtComment.CommentType.INLINE);
-
 						ampMethods.addAll(methodsWithTargetType.stream()
 								.map(addMth -> addInvocation(methodClone, addMth, target, localVar))
 								.collect(Collectors.toList()));
 					}
 				});
 		return ampMethods;
+	}
+
+	private void replaceInvocationByLocalVariable(CtStatement invocationToBeReplaced, CtLocalVariable localVariable) {
+		if (invocationToBeReplaced.getParent() instanceof CtBlock) {
+			invocationToBeReplaced.replace(localVariable);
+		} else {
+			CtElement parent = invocationToBeReplaced.getParent();
+			while (!(parent.getParent() instanceof CtBlock)) {
+				parent = invocationToBeReplaced.getParent();
+			}
+			((CtStatement) parent).insertBefore(localVariable);
+			invocationToBeReplaced.replace(localVariable.getReference());
+		}
+	}
+
+	private int getIndexOf(CtMethod originalMethod, CtInvocation originalInvocation) {
+		final List<CtStatement> statements = originalMethod.getElements(new TypeFilter<>(CtStatement.class));
+		int i;
+		for (i = 0; i < statements.size(); i++) {
+			if (statements.get(i) == originalInvocation) {
+				return i;
+			}
+		}
+		throw new RuntimeException("Could not find the statement: " +
+				originalInvocation.toString() +
+				" in " +
+				originalMethod.getBody().toString());
 	}
 
 	private List<CtLocalVariable<?>> getExistingObjects(CtMethod method) {
@@ -126,14 +154,14 @@ public class StatementAdd implements Amplifier {
 		List<CtExpression<?>> arguments = new ArrayList<>(parameters.size());
 
 		methodToInvokeToAdd.getParameters().forEach(parameter -> {
-					try {
-						CtLocalVariable<?> localVariable = ValueCreator.createRandomLocalVar(parameter.getType(), parameter.getSimpleName());
-						body.insertBegin(localVariable);
-						arguments.add(factory.createVariableRead(localVariable.getReference(), false));
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				});
+			try {
+				CtLocalVariable<?> localVariable = ValueCreator.createRandomLocalVar(parameter.getType(), parameter.getSimpleName());
+				body.insertBegin(localVariable);
+				arguments.add(factory.createVariableRead(localVariable.getReference(), false));
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
 		CtExpression targetClone = target.clone();
 		CtInvocation newInvocation = factory.Code().createInvocation(targetClone, methodToInvokeToAdd.getReference(), arguments);
 		DSpotUtils.addComment(newInvocation, "StatementAdd: add invocation of a method", CtComment.CommentType.INLINE);
@@ -141,11 +169,11 @@ public class StatementAdd implements Amplifier {
 		return methodClone;
 	}
 
-	private CtStatement findInvocationIn(CtMethod method, CtStatement invocation) {
-		List<CtStatement> statements = Query.getElements(method, new TypeFilter(CtStatement.class));
-		return statements.stream()
-				.filter(s -> s.toString().equals(invocation.toString()))
-				.findFirst().orElse(null);
+	private CtStatement findInvocationIn(CtMethod method, CtInvocation invocation) {
+		return method.getElements(new TypeFilter<>(CtInvocation.class))
+				.stream()
+				.filter(invocation1 -> invocation1 == invocation)
+				.findFirst().orElseThrow(RuntimeException::new);
 	}
 
 	private CtExpression<?> createLocalVarRef(CtLocalVariable var) {
