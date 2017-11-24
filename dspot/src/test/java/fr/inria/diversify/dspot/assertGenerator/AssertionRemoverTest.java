37a1e97678b9ea8cbe7d05abb789b4cf1dc2e5ce
danglotb <bdanglot@gmail.com>
2017-11-24 10:49:45 +0100

    test: add test on assertion remover for assertions with unary and inside case: statement

--- dspot/src/test/java/fr/inria/diversify/dspot/assertGenerator/AssertionRemoverTest.java
+++ dspot/src/test/java/fr/inria/diversify/dspot/assertGenerator/AssertionRemoverTest.java
@@ -2,14 +2,11 @@ package fr.inria.diversify.dspot.assertGenerator;
 
 import fr.inria.diversify.Utils;
 import fr.inria.diversify.dspot.AbstractTest;
-import fr.inria.diversify.utils.logging.AssertionRemover;
+import fr.inria.diversify.utils.AmplificationChecker;
 import org.junit.Test;
-import spoon.processing.AbstractProcessor;
-import spoon.processing.ProcessingManager;
+import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.factory.Factory;
-import spoon.support.QueueProcessingManager;
+import spoon.reflect.visitor.filter.TypeFilter;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,33 +18,77 @@ import static org.junit.Assert.assertEquals;
  */
 public class AssertionRemoverTest extends AbstractTest {
 
-
 	@Test
-	public void testRemoveAssertion() throws Exception {
-
-		/*
-			Test that the AssertionRemover remove assertions from tests
-		 */
-
-		final CtClass aClass = Utils.findClass("fr.inria.assertionremover.TestClassWithAssertToBeRemoved");
-
-		final AbstractProcessor<CtMethod> assertionRemover = new AssertionRemover();
-		final Factory factory = Utils.getFactory();
-		final ProcessingManager processingManager = new QueueProcessingManager(factory);
-		processingManager.addProcessor(assertionRemover);
-		processingManager.process(aClass);
+	public void testRemoveAssertionOnSimpleExample() throws Exception {
+		final CtClass<?> testClass = Utils.findClass("fr.inria.sample.TestClassWithAssert");
+		final AssertionRemover assertionRemover = new AssertionRemover();
+		testClass.getMethodsByName("test1").get(0).getElements(new TypeFilter<CtInvocation>(CtInvocation.class) {
+			@Override
+			public boolean matches(CtInvocation element) {
+				return AmplificationChecker.isAssert(element);
+			}
+		}).forEach(assertionRemover::removeAssertion);
 
 		final String expectedMethod = "@org.junit.Test" + nl +
 				"public void test1() {" + nl +
 				"    fr.inria.sample.ClassWithBoolean cl = new fr.inria.sample.ClassWithBoolean();" + nl +
-				"    java.lang.Object o0 = cl.getTrue();" + nl +
+				"    boolean boolean_0 = cl.getTrue();" + nl +
+				"}";
+		assertEquals(expectedMethod , testClass.getMethodsByName("test1").get(0).toString());
+	}
+
+	@Test
+	public void testRemoveAssertionWithSwitchCase() throws Exception {
+
+		/*
+			Test that the AssertionRemover remove assertions from tests when the assertion is inside a case:
+		 */
+
+		final CtClass<?> testClass = Utils.findClass("fr.inria.assertionremover.TestClassWithAssertToBeRemoved");
+		final AssertionRemover assertionRemover = new AssertionRemover();
+		testClass.getMethodsByName("test1").get(0).getElements(new TypeFilter<CtInvocation>(CtInvocation.class) {
+			@Override
+			public boolean matches(CtInvocation element) {
+				return AmplificationChecker.isAssert(element);
+			}
+		}).forEach(assertionRemover::removeAssertion);
+
+		final String expectedMethod = "@org.junit.Test" + nl +
+				"public void test1() {" + nl +
+				"    fr.inria.sample.ClassWithBoolean cl = new fr.inria.sample.ClassWithBoolean();" + nl +
+				"    boolean boolean_0 = cl.getTrue();" + nl +
 				"    int one = 1;" + nl +
 				"    switch (one) {" + nl +
 				"        case 1 :" + nl +
-				"            java.lang.Object o1 = fr.inria.assertionremover.TestClassWithAssertToBeRemoved.getNegation(cl.getFalse());" + nl +
+				"            boolean boolean_1 = fr.inria.assertionremover.TestClassWithAssertToBeRemoved.getNegation(cl.getFalse());" + nl +
 				"            break;" + nl +
 				"    }" + nl +
 				"}";
-		assertEquals(expectedMethod , aClass.getMethodsByName("test1").get(0).toString());
+		assertEquals(expectedMethod , testClass.getMethodsByName("test1").get(0).toString());
 	}
+
+	@Test
+	public void testRemoveAssertionWithUnary() throws Exception {
+
+		/*
+			Test that the AssertionRemover remove assertions from tests when assertions used unary operators
+		 */
+
+		final CtClass<?> testClass = Utils.findClass("fr.inria.assertionremover.TestClassWithAssertToBeRemoved");
+		final AssertionRemover assertionRemover = new AssertionRemover();
+		testClass.getMethodsByName("test2").get(0).getElements(new TypeFilter<CtInvocation>(CtInvocation.class) {
+			@Override
+			public boolean matches(CtInvocation element) {
+				return AmplificationChecker.isAssert(element);
+			}
+		}).forEach(assertionRemover::removeAssertion);
+
+		final String expectedMethod = "@org.junit.Test" + nl +
+				"public void test2() throws java.lang.Exception {" + nl +
+				"    int a = -1;" + nl +
+				"    int b = 1;" + nl +
+				"}";
+		assertEquals(expectedMethod , testClass.getMethodsByName("test2").get(0).toString());
+	}
+
 }
