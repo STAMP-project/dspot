2d6ebde14266fd1c600670d372ca59998c9c0bbe
danglotb <bdanglot@gmail.com>
2018-02-05 13:03:16 +0100

    resources: add a test class resource

---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/factory/FactoryTest.java
@@ -0,0 +1,32 @@
+package fr.inria.factory;
+
+import org.junit.Test;
+
+public class FactoryTest {
+
+    public static class aClass {
+        private int x;
+        private aClass(int x) {
+            this.x = x;
+        }
+        private aClass(String s) {
+            this.x = s.length();
+        }
+        public int getX() {
+            return x;
+        }
+    }
+
+    public static aClass createAClass() {
+        return new aClass(1);
+    }
+
+    public static aClass build(int x) {
+        return new aClass(x);
+    }
+
+    @Test
+    public void test() throws Exception {
+        final aClass aClass = createAClass();
+    }
+}
