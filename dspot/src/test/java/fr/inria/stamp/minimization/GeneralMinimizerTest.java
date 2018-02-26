b1e0223dfa7b301a15654e7bedf3e8fa49b0ebb7
danglotb <bdanglot@gmail.com>
2018-02-26 22:07:23 +0100

    test: add a test on GeneralMinimizer

---
+++ dspot/src/test/java/fr/inria/stamp/minimization/GeneralMinimizerTest.java
@@ -0,0 +1,35 @@
+package fr.inria.stamp.minimization;
+
+import fr.inria.diversify.Utils;
+import fr.inria.diversify.dspot.AbstractTest;
+import fr.inria.diversify.utils.AmplificationHelper;
+import org.junit.Test;
+import spoon.reflect.declaration.CtMethod;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 26/02/18
+ */
+public class GeneralMinimizerTest extends AbstractTest {
+
+    @Test
+    public void testMinimize() throws Exception {
+
+        /*
+            The GeneralMinimizer is not directed by any test-criterion.
+            The general minimization is done using static analysis of the code.
+            Find a list of minimization done:
+                - redundant of local variable (e.g. a literal that just used one time can be in-lined)
+         */
+
+        final GeneralMinimizer generalMinimizer = new GeneralMinimizer();
+        final CtMethod<?> minimize = generalMinimizer.minimize(Utils.findMethod("fr.inria.amplified.AmplifiedTest", "amplifiedTest"));
+        final String expectedBody = "{" + AmplificationHelper.LINE_SEPARATOR +
+                "assertEquals(5, 5);" + AmplificationHelper.LINE_SEPARATOR +
+                "}";
+        assertEquals(expectedBody, minimize.getBody().toString());
+    }
+}
---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/amplified/AmplifiedTest.java
@@ -0,0 +1,19 @@
+package fr.inria.amplified;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 26/02/18
+ */
+public class AmplifiedTest {
+
+    @Test
+    public void amplifiedTest() throws Exception {
+        int integer5 = 5;
+        assertEquals(5, integer5);
+    }
+}
