ce30876a37b9af5f3d492be08d70ccf0e785ea84
danglotb <bdanglot@gmail.com>
2018-02-26 22:07:45 +0100

    feat: hierarchy of minimizers

---
+++ dspot/src/main/java/fr/inria/stamp/minimization/GeneralMinimizer.java
@@ -0,0 +1,18 @@
+package fr.inria.stamp.minimization;
+
+import spoon.reflect.declaration.CtMethod;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 26/02/18
+ */
+public class GeneralMinimizer implements Minimizer {
+
+    @Override
+    public CtMethod<?> minimize(CtMethod<?> amplifiedTestToBeMinimized) {
+        // TODO implement
+        return amplifiedTestToBeMinimized;
+    }
+
+}
---
+++ dspot/src/main/java/fr/inria/stamp/minimization/PitMutantMinimizer.java
@@ -0,0 +1,17 @@
+package fr.inria.stamp.minimization;
+
+import spoon.reflect.declaration.CtMethod;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 26/02/18
+ */
+public class PitMutantMinimizer extends GeneralMinimizer {
+
+    @Override
+    public CtMethod<?> minimize(CtMethod<?> amplifiedTestToBeMinimized) {
+        // TODO implement
+        return super.minimize(amplifiedTestToBeMinimized);
+    }
+}
