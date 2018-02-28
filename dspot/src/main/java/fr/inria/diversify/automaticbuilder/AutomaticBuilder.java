78a1b7db57ac04c2532e86cc3c7abf354b84f673
danglotb <bdanglot@gmail.com>
2018-02-28 11:09:34 +0100

    remove depreciation of compile().

--- dspot/src/main/java/fr/inria/diversify/automaticbuilder/AutomaticBuilder.java
+++ dspot/src/main/java/fr/inria/diversify/automaticbuilder/AutomaticBuilder.java
@@ -9,11 +9,8 @@ import spoon.reflect.declaration.CtType;
  */
 public interface AutomaticBuilder {
 
-    @Deprecated
     void compile(String pathToRootOfProject);
 
-    @Deprecated // TODO this method will be replaced by a paramters of the main of DSpot.
-    // TODO the idea is to extract this information for the usage as a maven plugin
     String buildClasspath(String pathToRootOfProject);
 
     void reset();
