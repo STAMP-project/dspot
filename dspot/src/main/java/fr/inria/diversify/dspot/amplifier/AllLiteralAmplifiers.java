3ca4cfa7fafc16419d3e98a77321a8bb75373f71
danglotb <bdanglot@gmail.com>
2018-03-07 13:19:54 +0100

    feat: refactor the way that All Literal Amplifiers are done, it is now a specific Amplifier, rather than a list

---
+++ dspot/src/main/java/fr/inria/diversify/dspot/amplifier/AllLiteralAmplifiers.java
@@ -0,0 +1,39 @@
+package fr.inria.diversify.dspot.amplifier;
+
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 07/03/18
+ */
+public class AllLiteralAmplifiers implements Amplifier {
+
+    private List<Amplifier> literalAmplifiers;
+
+    public AllLiteralAmplifiers() {
+        this.literalAmplifiers = Arrays.asList(
+                new StringLiteralAmplifier(),
+                new NumberLiteralAmplifier(),
+                new BooleanLiteralAmplifier(),
+                new CharLiteralAmplifier()
+        );
+    }
+
+    @Override
+    public List<CtMethod> apply(CtMethod testMethod) {
+        return this.literalAmplifiers.stream()
+                .flatMap(amplifier -> amplifier.apply(testMethod).stream())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void reset(CtType testClass) {
+        this.literalAmplifiers.forEach(amplifier -> amplifier.reset(testClass));
+    }
+}
