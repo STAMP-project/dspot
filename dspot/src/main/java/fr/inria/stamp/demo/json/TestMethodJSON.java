1355c5d5bc511360f512032924c6a2d360716be7
danglotb <bdanglot@gmail.com>
2018-03-13 00:32:47 +0100

    wip: instrumentation to collect data for the demo

---
+++ dspot/src/main/java/fr/inria/stamp/demo/json/TestMethodJSON.java
@@ -0,0 +1,14 @@
+package fr.inria.stamp.demo.json;
+
+public class TestMethodJSON {
+
+    public final String clazz;
+    public final String method;
+    public final String source_code;
+
+    public TestMethodJSON(String clazz, String method, String source_code) {
+        this.clazz = clazz;
+        this.method = method;
+        this.source_code = source_code;
+    }
+}
