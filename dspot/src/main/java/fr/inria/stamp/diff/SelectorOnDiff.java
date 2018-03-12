ff9aba49e2ca25f34aebaf947cb5d45ce3aec775
danglotb <bdanglot@gmail.com>
2018-03-12 13:30:01 +0100

    feat: dspot select test to be amplified according to a diff

---
+++ dspot/src/main/java/fr/inria/stamp/diff/SelectorOnDiff.java
@@ -0,0 +1,164 @@
+package fr.inria.stamp.diff;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+import fr.inria.diversify.utils.AmplificationChecker;
+import fr.inria.diversify.utils.AmplificationHelper;
+import fr.inria.diversify.utils.sosiefier.InputConfiguration;
+import gumtree.spoon.AstComparator;
+import gumtree.spoon.diff.Diff;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.reference.CtExecutableReference;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 08/03/18
+ */
+public class SelectorOnDiff {
+
+    public static int MAX_NUMBER_TEST_CLASSES = 5;
+
+    private static int MAX_NUMBER_TEST_CASES = 20;
+
+    public static List<CtType> findTestClassesAccordingToADiff(InputConfiguration configuration) {
+        final Factory factory = configuration.getInputProgram().getFactory();
+        final String baseSha = configuration.getProperties().getProperty("baseSha");
+        final String pathToFirstVersion = configuration.getProperties().getProperty("project") +
+                (configuration.getProperties().getProperty("targetModule") != null ?
+                        configuration.getProperties().getProperty("targetModule") != null : "");
+        final String pathToSecondVersion = configuration.getProperties().getProperty("folderPath") +
+                (configuration.getProperties().getProperty("targetModule") != null ?
+                        configuration.getProperties().getProperty("targetModule") != null : "");
+        // TODO must use the configuration to compute path to src and testSrc, in case of non-standard path
+        return findTestClassesAccordingToADiff(factory, baseSha,
+                pathToFirstVersion,
+                pathToSecondVersion
+        );
+    }
+
+    private static List<CtType> findTestClassesAccordingToADiff(Factory factory,
+                                                                 String baseSha,
+                                                                 String pathToFirstVersion,
+                                                                 String pathToSecondVersion) {
+        //get the modified methods
+        final Set<String> modifiedJavaFiles = pathToModifiedJavaFile(baseSha, pathToSecondVersion);
+
+        // keep modified test in the PR: must be present in both versions of the program
+        final List<CtType> modifiedTestClasses =
+                getModifiedTestClasses(factory, pathToFirstVersion, pathToSecondVersion, modifiedJavaFiles);
+        if (!modifiedTestClasses.isEmpty()) {
+            return reduceIfNeeded(modifiedTestClasses);
+        }
+        // find all modified methods
+        final Set<CtMethod> modifiedMethods = modifiedJavaFiles.stream()
+                .flatMap(s ->
+                        getModifiedMethod(
+                                pathToFirstVersion + s.substring(1),
+                                pathToSecondVersion + s.substring(1)
+                        ).stream()
+                ).collect(Collectors.toSet());
+        //find all test classes that execute those methods
+        final List<CtType> selectedTestClasses = factory.Package().getRootPackage()
+                .getElements(new TypeFilter<CtExecutableReference>(CtExecutableReference.class) {
+                    @Override
+                    public boolean matches(CtExecutableReference element) {
+                        return modifiedMethods.contains(element.getDeclaration());
+                    }
+                }).stream()
+                .map(ctExecutableReference -> ctExecutableReference.getParent(CtType.class))
+                .filter(ctType -> ctType.getMethods()
+                        .stream()
+                        .anyMatch(method -> {
+                                    try {
+                                        return AmplificationChecker.isTest((CtMethod<?>) method);
+                                    } catch (Exception e) {
+                                        return false;
+                                    }
+                                }
+                        )
+                ).collect(Collectors.toList());
+        // TODO we may need another way to limit the number of used tests
+        // TODO we can use the number of test classes
+        // TODO or we could use the number of test cases
+        return reduceIfNeeded(selectedTestClasses);
+    }
+
+    private static List<CtType> reduceIfNeeded(List<CtType> selectedTestClasses) {
+        if (selectedTestClasses.size() > MAX_NUMBER_TEST_CLASSES) {
+            Collections.shuffle(selectedTestClasses, AmplificationHelper.getRandom());
+            return selectedTestClasses.subList(0, MAX_NUMBER_TEST_CLASSES);
+        } else {
+            return selectedTestClasses;
+        }
+    }
+
+    private static List<CtType> getModifiedTestClasses(Factory factory,
+                                                       String pathToFirstVersion,
+                                                       String pathToSecondVersion,
+                                                       Set<String> modifiedJavaFiles) {
+        return modifiedJavaFiles.stream()
+                .filter(pathToClass ->
+                        new File(pathToFirstVersion + pathToClass.substring(1)).exists() &&
+                        new File(pathToSecondVersion + pathToClass.substring(1)).exists() // it is present in both versions
+                ).filter(pathToClass -> {
+                    final String[] split = pathToClass.split("/");
+                    return (split[split.length - 1].split("\\.")[0].endsWith("Test") || // the class in a test class
+                            split[split.length - 1].split("\\.")[0].startsWith("Test"));
+                })
+                .map(pathToClass -> pathToClass.substring("a/src/main/java/".length()).split("\\.")[0].replace("/", "."))// TODO maybe be more flexible on the src/main/java (use the InputConfiguration)
+                .map(nameOfModifiedTestClass -> factory.Class().get(nameOfModifiedTestClass))
+                .collect(Collectors.toList());
+    }
+
+    private static Set<CtMethod> getModifiedMethod(String pathFile1, String pathFile2) {
+        try {
+            final File file1 = new File(pathFile1);
+            final File file2 = new File(pathFile2);
+            if (!file1.exists() || !file2.exists()) {
+                return Collections.emptySet();
+            }
+            Diff result = (new AstComparator()).compare(file1, file2);
+            return result.getRootOperations()
+                    .stream()
+                    .map(operation -> operation.getSrcNode().getParent(CtMethod.class))
+                    .collect(Collectors.toSet());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Set<String> pathToModifiedJavaFile(String baseSha, String pathToChangedVersion) {
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec(
+                    "git diff " + baseSha,
+                    new String[]{},
+                    new File(pathToChangedVersion));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        final Set<String> modifiedJavaFiles = new BufferedReader(new InputStreamReader(p.getInputStream()))
+                .lines()
+                .filter(line -> line.startsWith("diff") && line.endsWith(".java"))
+                .map(line -> line.split(" ")[2])
+                .collect(Collectors.toSet());
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return modifiedJavaFiles;
+    }
+
+}
