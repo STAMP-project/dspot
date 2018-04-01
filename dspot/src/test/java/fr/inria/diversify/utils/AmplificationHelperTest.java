8d392d6f9356fee08ed3bcdaf26363c97e5ece28
danglotb <bdanglot@gmail.com>
2018-04-01 17:02:39 +0200

    test: add a test on conversion of a JUnit3 test class into JUnit4 test class

--- dspot/src/test/java/fr/inria/diversify/utils/AmplificationHelperTest.java
+++ dspot/src/test/java/fr/inria/diversify/utils/AmplificationHelperTest.java
@@ -2,6 +2,7 @@ package fr.inria.diversify.utils;
 
 import fr.inria.Utils;
 import fr.inria.AbstractTest;
+import fr.inria.stamp.ConverterJUnit3ToJUnit4;
 import org.junit.Test;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.declaration.*;
@@ -20,6 +21,47 @@ import static org.junit.Assert.*;
  * on 1/30/17
  */
 public class AmplificationHelperTest extends AbstractTest {
+
+    @Test
+    public void testConvert() throws Exception {
+        final CtClass testClassJUnit3 = Utils.findClass("fr.inria.helper.ClassJunit3");
+        final CtType<?> converted = AmplificationHelper.convert(testClassJUnit3);
+        System.out.println(converted);
+        assertEquals("public class ClassJunit3 {\n" +
+                        "    class MyInnerClass {\n" +
+                        "        int value;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @org.junit.Test(timeout = 10000)\n" +
+                        "    public void test() throws java.lang.Exception {\n" +
+                        "        fr.inria.helper.ClassJunit3.MyInnerClass innerClass = new fr.inria.helper.ClassJunit3.MyInnerClass();\n" +
+                        "        innerClass.value = 4;\n" +
+                        "        org.junit.Assert.assertEquals(4, innerClass.value);\n" +
+                        "    }\n" +
+                        "}",
+                converted.toString());
+
+        final CtClass secondTestClassJUnit3 = Utils.findClass("fr.inria.helper.SecondClassJUnit3");
+        final CtType<?> secondConverted = AmplificationHelper.convert(secondTestClassJUnit3);
+        System.out.println(secondConverted);
+        assertEquals("/**\n" +
+                        " * Created by Benjamin DANGLOT\n" +
+                        " * benjamin.danglot@inria.fr\n" +
+                        " * on 01/04/18\n" +
+                        " */\n" +
+                        "public class SecondClassJUnit3 {\n" +
+                        "    @org.junit.Test(timeout = 10000)\n" +
+                        "    public void test() throws java.lang.Exception {\n" +
+                        "        junit.framework.TestCase.assertEquals(3, 3);\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @org.junit.Test(timeout = 10000)\n" +
+                        "    public void should() throws java.lang.Exception {\n" +
+                        "        junit.framework.TestCase.assertTrue(true);\n" +
+                        "    }\n" +
+                        "}",
+                secondConverted.toString());
+    }
 
     @Test
     public void testCreateAmplifiedTestClass() throws Exception {
