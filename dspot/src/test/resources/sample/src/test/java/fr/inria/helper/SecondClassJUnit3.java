94399c90424aae154b6e37f1a5c289c46c93477a
danglotb <bdanglot@gmail.com>
2018-04-01 17:01:52 +0200

    test resources: update test resources to have a Second JUnit3 test class

---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/helper/SecondClassJUnit3.java
@@ -0,0 +1,20 @@
+package fr.inria.helper;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 01/04/18
+ */
+public class SecondClassJUnit3 extends TestCase {
+
+    public void test() {
+        assertEquals(3, 3);
+    }
+
+    public void should() {
+        assertTrue(true);
+    }
+
+}
