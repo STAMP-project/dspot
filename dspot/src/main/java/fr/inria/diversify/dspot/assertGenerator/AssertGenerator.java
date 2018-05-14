cc839d9005e6783f11ed47b957620540055a19cd
danglotb <bdanglot@gmail.com>
2018-05-14 13:56:32 +0200

    fix: remove unused import

--- dspot/src/main/java/fr/inria/diversify/dspot/assertGenerator/AssertGenerator.java
+++ dspot/src/main/java/fr/inria/diversify/dspot/assertGenerator/AssertGenerator.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
