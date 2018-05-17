76a59f8a18599d0ee3933bac54431482166205fd
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-05-17 11:32:06 +0200

    ci: deploy a snapshot when the build succeed (#437)

---
+++ .travis/travis-deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+    cp .travis/travis-settings.xml $HOME/.m2/settings.xml && mvn deploy -DskipTests
+else
+    echo "Nothing to deploy when on PR or other branch"
+fi
