72e481dc5bd3b36deb5be84480b46ff631326cbd
danglotb <bdanglot@gmail.com>
2018-05-27 11:23:34 +0200

    clean: remove old methods, fields and properties from sosiefier

--- dspot/src/main/java/eu/stamp_project/utils/sosiefier/InputConfiguration.java
+++ dspot/src/main/java/eu/stamp_project/utils/sosiefier/InputConfiguration.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
@@ -28,10 +26,6 @@ import java.util.stream.Collectors;
 //TODO @Deprecated
 public class InputConfiguration {
 
-    //GENERATOR VERSIONS
-    public static final String GEN_VERSION_1_0_0 = "1.0.0";
-    public static final String LATEST_GENERATOR_VERSION = GEN_VERSION_1_0_0;
-
     /**
      * Internal properties
      */
@@ -40,36 +34,18 @@ public class InputConfiguration {
     /**
      * resulting input program from the input configuration
      */
+    @Deprecated
     private InputProgram inputProgram;
-
-    /*
-     * Output path to all operations resulting in output
-     */
-    //private String outputPath;
-
-    /**
-     * Errors raised during validation
-     */
-    private List<String> errors;
-
-    private String rootPath;
-
 
     public InputConfiguration() {
         prop = new Properties();
         setDefaultProperties();
-        setCodeFragmentClass();
-        errors = new ArrayList<>();
-        rootPath = prop.getProperty("root.path");
     }
 
     public InputConfiguration(InputStream stream) throws IOException {
         prop = new Properties();
         setDefaultProperties();
         prop.load(stream);
-        setCodeFragmentClass();
-        errors = new ArrayList<>();
-        rootPath = prop.getProperty("root.path");
         if (prop.getProperty("systemProperties") != null) {
             Arrays.stream(prop.getProperty("systemProperties").split(","))
                     .forEach( systemProperty -> {
@@ -94,7 +70,6 @@ public class InputConfiguration {
 			getProperties().setProperty("filter", filter);
 		}
 		getProperties().setProperty("javaVersion", "8");
-		rootPath = project.getAbsolutePath();
 	}
 
     private String getRelativePath(File path) {
@@ -105,9 +80,6 @@ public class InputConfiguration {
 
     public InputConfiguration(String file) throws IOException {
         this(new FileInputStream(file));
-        if (rootPath == null || rootPath.equals("")) {
-            rootPath = System.getProperty("user.dir");
-        }
     }
 
     /**
@@ -143,6 +115,7 @@ public class InputConfiguration {
     /**
      * The input program we are sosieficating.
      */
+    @Deprecated
     public InputProgram getInputProgram() {
         return inputProgram;
     }
@@ -150,6 +123,7 @@ public class InputConfiguration {
     /**
      * The input program we are sosieficating.
      */
+    @Deprecated
     public void setInputProgram(InputProgram inputProgram) {
         this.inputProgram = inputProgram;
     }
@@ -183,32 +157,12 @@ public class InputConfiguration {
 
 
     /**
-     * Returns the path of the previously found transformations
-     *
-     * @return
-     */
-    public String getPreviousTransformationPath() {
-        String s = prop.getProperty("transformation.directory", "");
-        return getAbsolutePath(s);
-    }
-
-    /**
      * Returns the path of the built classes
      *
      * @return String with the path
      */
     public String getClassesDir() {
         return  prop.getProperty("classes");
-    }
-
-    /**
-     * Get coverage dir of a project
-     *
-     * @return
-     */
-    public String getCoverageDir() {
-        String s = prop.getProperty("jacoco") == null ? prop.getProperty("coverage", "") : prop.getProperty("jacoco");
-        return getAbsolutePath(s);
     }
 
     /**
@@ -219,87 +173,15 @@ public class InputConfiguration {
         return prop.getProperty("outputDirectory", "output");
     }
 
-    /**
-     * If some path is given as relative path, this root will be used to resolve it
-     */
-    public String getRootPath() {
-        return rootPath;
-    }
-
-    /**
-     * Errors found during the validation
-     *
-     * @return A collection of messages of errors found during the validation
-     */
-    public Collection<String> getValidationErrors() {
-        return errors;
-    }
-
-  /*  *//**
-     * Returns a new code fragment given the processing level set by the user in the input properties
-     * <p>
-     * Defaults to statement in case of any error
-     *
-     * @return
-     *//*
-    public CodeFragment getNewCodeFragment() {
-        CodeFragment result;
-        try {
-            Class cl = Class.forName(getProperties().getProperty("CodeFragmentClass"));
-            result = (CodeFragment) cl.newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            result = new Statement();
-            Log.warn("Unable to create the code fragment requested, resolved to Statement level", e);
-            e.printStackTrace();
-        }
-        return result;
-    }*/
-
-    protected void setCodeFragmentClass() {
-        if (getProperties().getProperty("processor").equals("eu.stamp_project.diversify.codeFragmentProcessor.StatementProcessor")) {
-            getProperties().setProperty("CodeFragmentClass", "eu.stamp_project.diversify.codeFragment.Statement");
-        }
-        if (getProperties().getProperty("processor").equals("eu.stamp_project.diversify.codeFragmentProcessor.ExpressionProcessor")) {
-            getProperties().setProperty("CodeFragmentClass", "eu.stamp_project.diversify.codeFragment.Expression");
-        }
-        if (getProperties().getProperty("processor").equals("eu.stamp_project.diversify.codeFragmentProcessor.BlockProcessor")) {
-            getProperties().setProperty("CodeFragmentClass", "eu.stamp_project.diversify.codeFragment.Block");
-        }
-    }
-
     protected void setDefaultProperties() {
         prop.setProperty("src", "src/main/java");
         prop.setProperty("testSrc", "src/test/java");
         prop.setProperty("classes", "target/classes");
-        prop.setProperty("clojure", "false");
         prop.setProperty("javaVersion", "5");
-        prop.setProperty("transformation.type", "all");
-        prop.setProperty("transformation.size", "1");
-        prop.setProperty("stat", "false");
-        prop.setProperty("sosie", "false");
-
-        //Indicates if the Diversify must report early
-        prop.setProperty("early.report", "0");
-
-        //temporary directory
-        prop.setProperty("tmpDir", "tmpDir");
-
-        //directory for output
+        prop.setProperty("tmpDir", "tmpDir"); // TODO Checks usage
         prop.setProperty("outputDirectory", "output");
-
-        prop.setProperty("sosieOnMultiProject", "false");
-        prop.setProperty("timeOut", "-1");
-        prop.setProperty("logLevel", "2");
-        prop.setProperty("gitRepository", "null");
-        prop.setProperty("processor", "eu.stamp_project.diversify.codeFragmentProcessor.StatementProcessor");
-        prop.setProperty("transplant.point.search.strategy",
-                "eu.stamp_project.diversify.transformation.executeQuery.searchStrategy.SimpleRandomStrategy");
-        prop.setProperty("transplant.search.strategy",
-                "eu.stamp_project.diversify.transformation.executeQuery.searchStrategy.SimpleRandomStrategy");
-
-        prop.setProperty("syncroRange", "0");
-        prop.setProperty("maven.newPomFile", "");
-        prop.setProperty("transformation.level", "statement");
+        prop.setProperty("timeOut", "-1"); // TODO Checks usage
+        prop.setProperty("logLevel", "2"); // TODO Checks usage
         prop.setProperty("builder", "maven");
         prop.setProperty("pom", "/pom.xml");
     }
@@ -309,40 +191,7 @@ public class InputConfiguration {
         if ( new File(path).exists() || p.isAbsolute() ) {
             return path;
         }
-        if (rootPath != null && !rootPath.equals("")) {
-            p = Paths.get(rootPath + "/" + path);
-        } else {
-            p = Paths.get(path);
-        }
         return p.normalize().toString().replace(File.separator, "/");
-    }
-
-
-    private void addError(String error) {
-        errors.add(error);
-    }
-
-    private void checkPath(String desc, String path, boolean mustExist) {
-        if (path == null) {
-            if (mustExist) {
-                addError(desc + " at " + path + " does not exists");
-            }
-        } else if (!new File(path).exists() && (mustExist || !path.equals(""))) {
-            addError(desc + " at " + path + " does not exists");
-        }
-    }
-
-    /**
-     * Checks that the configuration is valid
-     */
-    public boolean validate() {
-        checkPath("Project path", getProjectPath(), true);
-        checkPath("Source path", getProjectPath() + "/"+ getOutputDirectory() , true);
-        checkPath("Previous transformation path", getPreviousTransformationPath(), false);
-        checkPath("Coverage dir", getCoverageDir(), false);
-        checkPath("Root dir", getRootPath(), false);
-        checkPath("Temp directory", getTempDir(), false);
-        return errors.size() == 0;
     }
 
     /**
@@ -394,15 +243,11 @@ public class InputConfiguration {
 			toReturn += key + ": " + prop.getProperty((String) key)+ "\n";
 		}
 		toReturn += "ClassesDir: " + this.getClassesDir()+ "\n";
-		toReturn += "coverageDir: " + this.getCoverageDir()+ "\n";
 		toReturn += "outputDirectory: " + this.getOutputDirectory()+ "\n";
-		toReturn += "previousTransformationPath: " + this.getPreviousTransformationPath()+ "\n";
 		toReturn += "projectPath: " + this.getProjectPath()+ "\n";
 		toReturn += "relativeSourceCodeDir: " + this.getRelativeSourceCodeDir()+ "\n";
 		toReturn += "relativeTestSourceCodeDir: " + this.getRelativeTestSourceCodeDir()+ "\n";
-		toReturn += "RootPath: " + this.getRootPath()+ "\n";
 		toReturn += "TempDir: " + this.getTempDir()+ "\n";
-		toReturn += "ValidationErrors: " + this.getValidationErrors();
 		return toReturn;
     }
 
