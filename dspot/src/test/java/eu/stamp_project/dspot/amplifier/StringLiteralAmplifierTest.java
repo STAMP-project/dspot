0435e694058727a1b316c56b7fa57ae8c5385333
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-05-31 13:13:46 +0200

    feat: add boolean to apply one time the specific value on each literals (#450)
    * feat: add boolean to apply one time the specific value on each literals
    
    * test: the amplifier should amplify less at the second iteration, and more after the reset

--- dspot/src/test/java/eu/stamp_project/dspot/amplifier/StringLiteralAmplifierTest.java
+++ dspot/src/test/java/eu/stamp_project/dspot/amplifier/StringLiteralAmplifierTest.java
@@ -16,6 +16,14 @@ public class StringLiteralAmplifierTest extends AbstractTest {
 
     @Test
     public void testAmplify() throws Exception {
+
+        /*
+            test the StringLiteral
+            The first iteration is complete, i.e. apply random operations plus the specific strings
+            the second iteration does not apply the specific strings, we already did
+            After the reset, the amplifier should obtain the same number than a the first iteration.
+         */
+
         final String nameMethod = "methodString";
         CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
         AmplificationHelper.setSeedRandom(42L);
@@ -23,7 +31,12 @@ public class StringLiteralAmplifierTest extends AbstractTest {
         amplificator.reset(literalMutationClass);
         CtMethod method = literalMutationClass.getMethod(nameMethod);
         List<CtMethod> mutantMethods = amplificator.apply(method);
-        assertEquals(46, mutantMethods.size());
+        assertEquals(43, mutantMethods.size());
+        mutantMethods = amplificator.apply(method);
+        assertEquals(23, mutantMethods.size());
+        amplificator.reset(literalMutationClass);
+        mutantMethods = amplificator.apply(method);
+        assertEquals(43, mutantMethods.size());
     }
 
     @Test
