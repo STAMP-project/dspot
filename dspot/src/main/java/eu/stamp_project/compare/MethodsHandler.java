4e2ab9a58d73c23096d9eeaa866d96cd1301b85a
danglotb <bdanglot@gmail.com>
2018-06-27 17:11:31 +0200

    fix: reduce the scope of methods to getters

--- dspot/src/main/java/eu/stamp_project/compare/MethodsHandler.java
+++ dspot/src/main/java/eu/stamp_project/compare/MethodsHandler.java
@@ -2,7 +2,11 @@ package eu.stamp_project.compare;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.BaseStream;
 
 
 public class MethodsHandler {
@@ -30,6 +34,8 @@ public class MethodsHandler {
         forbiddenMethods.add("listIterator");
         forbiddenMethods.add("stream");
         forbiddenMethods.add("parallelStream");
+        forbiddenMethods.add("reverse");
+        forbiddenMethods.add("clear");
     }
 
     public MethodsHandler() {
@@ -53,18 +59,40 @@ public class MethodsHandler {
         cache.put(clazz, methodsList);
     }
 
-    private boolean isValidMethod(Method m) {
-        if (!Modifier.isPublic(m.getModifiers())
-                || Modifier.isStatic(m.getModifiers())
-                || isVoid(m.getReturnType())
-                || m.getReturnType() == Class.class) {
+    private boolean isDefaulttoStringOrHashCode(Method method) {
+        return (method.getDeclaringClass().equals(Object.class) || method.getDeclaringClass().equals(Enum.class))
+                && (method.getName().equals("hashCode") || method.getName().equals("toString"));
+    }
+
+    private boolean isValidMethod(Method method) {
+        if (!Modifier.isPublic(method.getModifiers()) // the method is not public
+                || Modifier.isStatic(method.getModifiers()) // the method is static
+                || isVoid(method.getReturnType()) // the method is return void type, i.e. it returns nothing
+                || method.getReturnType() == Class.class // the method returns Class<?>
+                || !Modifier.isPublic(method.getReturnType().getModifiers())  // the method return a type that is not visible, i.e. is not public.
+                || returnStream(method) // the method return a stream
+                || method.getParameterTypes().length > 0 // the method hasParameter
+                || isDefaulttoStringOrHashCode(method) // we don't use default implementation of toString() and hashCode, i.e. implementation of Object and Enum
+                ) {
             return false;
         }
-        // we only consider methods that take no parameter
-        return (!forbiddenMethods.contains(m.getName()) ||
-                ( (!m.getDeclaringClass().equals(Object.class) && !m.getDeclaringClass().equals(Enum.class))
-                        && (m.getName().equals("hashCode") || m.getName().equals("toString"))))
-                        && m.getParameterTypes().length == 0;
+
+        // we rely on convention name: get, is, should
+        // TODO expand the scope of assertion to other pure method, with return type etc...
+
+        return method.getName().startsWith("get") ||
+                method.getName().startsWith("is") ||
+                method.getName().startsWith("should") ||
+                method.getName().equals("toString") ||
+                method.getName().startsWith("hashCode");
+    }
+
+    private boolean returnStream(Method method) {
+        try {
+            return BaseStream.class.isAssignableFrom(method.getReturnType());
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     private static boolean isVoid(Class<?> type) {
