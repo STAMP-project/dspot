2d5ece17d0da62efe0dcf011c8388e669a84828b
danglotb <bdanglot@gmail.com>
2018-06-27 11:25:15 +0200

    refactor: unify preAmplification and assertions amplification, better logging

--- dspot/src/main/java/eu/stamp_project/dspot/Amplification.java
+++ dspot/src/main/java/eu/stamp_project/dspot/Amplification.java
@@ -1,11 +1,9 @@
 package eu.stamp_project.dspot;
 
-import eu.stamp_project.testrunner.runner.test.Failure;
-import eu.stamp_project.testrunner.runner.test.TestListener;
 import eu.stamp_project.dspot.amplifier.Amplifier;
 import eu.stamp_project.dspot.assertGenerator.AssertGenerator;
 import eu.stamp_project.dspot.selector.TestSelector;
-import eu.stamp_project.utils.AmplificationChecker;
+import eu.stamp_project.testrunner.runner.test.TestListener;
 import eu.stamp_project.utils.AmplificationHelper;
 import eu.stamp_project.utils.DSpotUtils;
 import eu.stamp_project.utils.compilation.DSpotCompiler;
@@ -15,11 +13,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
-import spoon.reflect.declaration.ModifierKind;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -48,7 +44,14 @@ public class Amplification {
 
     private DSpotCompiler compiler;
 
-    public static int ampTestCount;
+    private int globalNumberOfSelectedAmplification;
+
+    /**
+     * @return the number of selected amplified test method. This number is counted over the whole run, <i>i.e.</i> potentially for multiple test classes.
+     */
+    public int getGlobalNumberOfSelectedAmplification() {
+        return globalNumberOfSelectedAmplification;
+    }
 
     public Amplification(InputConfiguration configuration, List<Amplifier> amplifiers, TestSelector testSelector, DSpotCompiler compiler) {
         this.configuration = configuration;
@@ -56,6 +59,7 @@ public class Amplification {
         this.testSelector = testSelector;
         this.compiler = compiler;
         this.assertGenerator = new AssertGenerator(this.configuration, this.compiler);
+        this.globalNumberOfSelectedAmplification = 0;
     }
 
     /**
@@ -76,34 +80,31 @@ public class Amplification {
      * <p>See {@link #amplification(CtType, CtMethod, int)} for the details of amplification.
      *
      * @param classTest    Test class
-     * @param methods      Methods to amplify
+     * @param tests        Methods to amplify
      * @param maxIteration Number of amplification iterations
      */
-    public void amplification(CtType<?> classTest, List<CtMethod<?>> methods, int maxIteration) {
-        List<CtMethod<?>> tests = methods.stream()
-                .filter(AmplificationChecker::isTest)
-                .collect(Collectors.toList());
-        if (tests.isEmpty()) {
-            LOGGER.warn("No test has been found into {}", classTest.getQualifiedName());
-            return;
-        }
-        LOGGER.info("amplification of {} ({} test(s))", classTest.getQualifiedName(), tests.size());
-        preAmplification(classTest, tests);
-        LOGGER.info("{} amplified test(s) has been selected, global: {}", this.testSelector.getAmplifiedTestCases().size() - ampTestCount, this.testSelector.getAmplifiedTestCases().size());
-        ampTestCount = this.testSelector.getAmplifiedTestCases().size();
+    public void amplification(CtType<?> classTest, List<CtMethod<?>> tests, int maxIteration) {
+        LOGGER.info("Amplification of {} ({} test(s))", classTest.getQualifiedName(), tests.size());
+        LOGGER.info("Assertion amplification of {} ({} test(s))", classTest.getQualifiedName(), tests.size());
+        final List<CtMethod<?>> passingTests = compileRunAndDiscardUncompilableAndFailingTestMethods(classTest, tests);
+        final List<CtMethod<?>> selectedToBeAmplified = this.testSelector.selectToAmplify(passingTests);
+        final List<CtMethod<?>> assertionAmplifiedTestMethods = this.assertionsAmplification(classTest, selectedToBeAmplified);
+        final List<CtMethod<?>> amplifiedTestMethodsToKeep = this.testSelector.selectToKeep(assertionAmplifiedTestMethods);
+        this.globalNumberOfSelectedAmplification += amplifiedTestMethodsToKeep.size();
+        LOGGER.info("{} amplified test methods has been selected to be kept. (global: {})", amplifiedTestMethodsToKeep.size(), this.globalNumberOfSelectedAmplification);
         // in case there is no amplifier, we can leave
         if (this.amplifiers.isEmpty()) {
             return;
         }
-        resetAmplifiers(classTest);
+        LOGGER.info("Applying Input-amplification and Assertion-amplification test by test.");
+        this.resetAmplifiers(classTest);
         for (int i = 0; i < tests.size(); i++) {
             CtMethod test = tests.get(i);
-            LOGGER.info("amp {} ({}/{})", test.getSimpleName(), i + 1, tests.size());
-            compileAndRunTests(classTest, Collections.singletonList(tests.get(i)));
-            amplification(classTest, test, maxIteration);
-            LOGGER.info("{} amplified test(s) has been selected, global: {}", this.testSelector.getAmplifiedTestCases().size() - ampTestCount, this.testSelector.getAmplifiedTestCases().size());
-            ampTestCount = this.testSelector.getAmplifiedTestCases().size();
-
+            LOGGER.info("Amplification of {}, ({}/{})", test.getSimpleName(), i + 1, tests.size());
+            //compileAndRunTests(classTest, Collections.singletonList(tests.get(i)));
+            final List<CtMethod<?>> amplifiedTestMethods = amplification(classTest, test, maxIteration);
+            this.globalNumberOfSelectedAmplification += amplifiedTestMethods.size();
+            LOGGER.info("{} amplified test methods has been selected to be kept. (global: {})", amplifiedTestMethodsToKeep.size(), this.globalNumberOfSelectedAmplification);
         }
     }
 
@@ -120,95 +121,49 @@ public class Amplification {
      * @return Valid amplified tests
      */
     private List<CtMethod<?>> amplification(CtType<?> classTest, CtMethod test, int maxIteration) {
+        // tmp list for current test methods to be amplified
+        // this list must be a implementation that support remove / clear methods
         List<CtMethod<?>> currentTestList = new ArrayList<>();
         currentTestList.add(test);
-
-        List<CtMethod<?>> amplifiedTests = new ArrayList<>();
-
+        // output
+        final List<CtMethod<?>> amplifiedTests = new ArrayList<>();
         for (int i = 0; i < maxIteration; i++) {
-            LOGGER.info("iteration {}:", i);
-            List<CtMethod<?>> testsToBeAmplified = testSelector.selectToAmplify(currentTestList);
-            if (testsToBeAmplified.isEmpty()) {
-                LOGGER.info("No test could be generated from selected test");
-                continue;
+            LOGGER.info("iteration {} / {}", i, maxIteration);
+            final List<CtMethod<?>> selectedToBeAmplified = testSelector.selectToAmplify(currentTestList);
+            if (selectedToBeAmplified.isEmpty()) {
+                LOGGER.warn("No test could be selected to be amplified.");
+                continue; // todo should we break the loop?
             }
             LOGGER.info("{} tests selected to be amplified over {} available tests",
-                    testsToBeAmplified.size(),
+                    selectedToBeAmplified.size(),
                     currentTestList.size()
             );
-            currentTestList = AmplificationHelper.reduce(inputAmplifyTests(testsToBeAmplified));
-            List<CtMethod<?>> testsWithAssertions = assertGenerator.assertionAmplification(classTest, currentTestList);
+            final List<CtMethod<?>> inputAmplifiedTests = this.inputAmplifyTests(selectedToBeAmplified);
+            final List<CtMethod<?>> reducedInputAmplifiedTests = AmplificationHelper.reduce(inputAmplifiedTests);
+            final List<CtMethod<?>> testsWithAssertions = this.assertionsAmplification(classTest, reducedInputAmplifiedTests);
+            // in case no test with assertions could be generated, we go for the next iteration.
             if (testsWithAssertions.isEmpty()) {
+                currentTestList = reducedInputAmplifiedTests;
                 continue;
-            } else {
-                currentTestList = testsWithAssertions;
             }
-            final TestListener result = compileAndRunTests(classTest, currentTestList);
-            if (result == null) {
-                continue;
-            } else if (!result.getFailingTests().isEmpty()) {
-                LOGGER.warn("Discarding failing test cases");
-                final Set<String> failingTestCase =
-                        result.getFailingTests()
-                                .stream()
-                                .map(failure -> failure.testCaseName)
-                                .collect(Collectors.toSet());
-                currentTestList = currentTestList.stream()
-                        .filter(ctMethod -> !failingTestCase.contains(ctMethod.getSimpleName()))
-                        .collect(Collectors.toList());
-            }
-            currentTestList = AmplificationHelper.getPassingTests(currentTestList, result);
-            LOGGER.info("{} test method(s) has been successfully generated", currentTestList.size());
-            amplifiedTests.addAll(testSelector.selectToKeep(currentTestList));
+            final List<CtMethod<?>> amplifiedTestMethodsToKeep = this.testSelector.selectToKeep(testsWithAssertions);
+            amplifiedTests.addAll(amplifiedTestMethodsToKeep);
+            LOGGER.info("{} amplified test methods has been selected to be kept.", amplifiedTestMethodsToKeep.size());
+            currentTestList = testsWithAssertions;
         }
         return amplifiedTests;
     }
 
-    /**
-     * Adds new assertions in multiple tests.
-     * <p>
-     * <p>Makes sure the test suite is valid before the assertion amplification {@link AssertGenerator#assertionAmplification(CtType, List)}.
-     *
-     * @param classTest Test class
-     * @param tests     New test methods
-     * @return Valid amplified tests
-     */
-    private List<CtMethod<?>> preAmplification(CtType classTest, List<CtMethod<?>> tests) {
-        TestListener result = compileAndRunTests(classTest, tests);
-        if (!result.getFailingTests().isEmpty()) {
-            LOGGER.warn("{} tests failed before the amplifications", result.getFailingTests().size());
-            LOGGER.warn("{}", result.getFailingTests().stream()
-                    .map(Object::toString)
-                    .collect(Collectors.joining(System.getProperty("line.separator")))
-            );
-            LOGGER.warn("Discarding following test cases for the amplification");
-            result.getFailingTests()
-                    .stream()
-                    .map(failure -> failure.testCaseName)
-                    .forEach(failure -> {
-                        try {
-                            CtMethod testToRemove = tests.stream()
-                                    .filter(m -> failure.equals(m.getSimpleName()))
-                                    .findFirst().get();
-                            tests.remove(tests.indexOf(testToRemove));
-                            LOGGER.warn("{}", testToRemove.getSimpleName());
-                        } catch (Exception ignored) {
-                            //ignored
-                        }
-                    });
-            return preAmplification(classTest, tests);
-        } else {
-            LOGGER.info("Try to add assertions before amplification");
-            final List<CtMethod<?>> amplifiedTestToBeKept = assertGenerator.assertionAmplification(
-                    classTest, testSelector.selectToAmplify(tests));
-            if (!amplifiedTestToBeKept.isEmpty()) {
-                compileAndRunTests(classTest, amplifiedTestToBeKept);
-                testSelector.selectToKeep(amplifiedTestToBeKept);
-                return testSelector.getAmplifiedTestCases();
-            } else {
-                return amplifiedTestToBeKept;
-            }
+    private List<CtMethod<?>> assertionsAmplification(CtType<?> classTest, List<CtMethod<?>> testMethods) {
+        List<CtMethod<?>> testsWithAssertions = this.assertGenerator.assertionAmplification(classTest, testMethods);
+        if (testsWithAssertions.isEmpty()) {
+            return testsWithAssertions;
         }
+        // final check on A-amplified test, see if they all pass.
+        // If they don't, we just discard them.
+        final List<CtMethod<?>> amplifiedPassingTests = compileRunAndDiscardUncompilableAndFailingTestMethods(classTest, testsWithAssertions);
+        LOGGER.info("Assertion amplification: {} test method(s) has been successfully amplified.", testMethods.size());
+        return amplifiedPassingTests;
     }
 
     /**
@@ -241,7 +196,7 @@ public class Amplification {
      */
     private Stream<CtMethod<?>> inputAmplifyTest(CtMethod<?> test) {
         final CtMethod topParent = AmplificationHelper.getTopParent(test);
-        return amplifiers.parallelStream()
+        return this.amplifiers.parallelStream()
                 .flatMap(amplifier -> amplifier.apply(test).stream())
                 .filter(amplifiedTest -> amplifiedTest != null && !amplifiedTest.getBody().getStatements().isEmpty())
                 .filter(distinctByKey(CtMethod::getBody))
@@ -251,59 +206,30 @@ public class Amplification {
     }
 
     private void resetAmplifiers(CtType parentClass) {
-        amplifiers.forEach(amp -> amp.reset(parentClass));
+        this.amplifiers.forEach(amp -> amp.reset(parentClass));
     }
 
     /**
-     * Adds test methods to the test class and run them.
-     *
+     * Create a clone of the test class, using {@link eu.stamp_project.utils.AmplificationHelper#cloneTestClassAndAddGivenTest(CtType, List)}.
+     * Then, compile and run the test using {@link eu.stamp_project.utils.compilation.TestCompiler#compileAndRun(CtType, DSpotCompiler, List, InputConfiguration)}
+     * Finally, discard all failing test methods
      * @param classTest       Test class
-     * @param currentTestList New test methods to run
+     * @param currentTestList test methods to be run
      * @return Results of tests' run
-     * @throws AmplificationException
+     * @throws AmplificationException forward the AmplificationException thrown by {@link eu.stamp_project.utils.compilation.TestCompiler#compileAndRun(CtType, DSpotCompiler, List, InputConfiguration)}
      */
-    private TestListener compileAndRunTests(CtType classTest, List<CtMethod<?>> currentTestList) {
+    private List<CtMethod<?>> compileRunAndDiscardUncompilableAndFailingTestMethods(CtType classTest, List<CtMethod<?>> currentTestList) {
         CtType amplifiedTestClass = AmplificationHelper.cloneTestClassAndAddGivenTest(classTest, currentTestList);
         try {
-            return TestCompiler.compileAndRun(
+            final TestListener result = TestCompiler.compileAndRun(
                     amplifiedTestClass,
                     this.compiler,
                     currentTestList,
                     this.configuration
             );
+            return AmplificationHelper.getPassingTests(currentTestList, result);
         } catch (AmplificationException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Adds test methods to the test class and run them. Does not allow partially failing test classes.
-     *
-     * @param classTest       Test class
-     * @param currentTestList New test methods to run
-     * @return Results of tests run or {@code null} if a test failed or could not be run (uncompilable)
-     */
-    public TestListener compileAndRunTestsNoFail(CtType classTest, List<CtMethod<?>> currentTestList)
-            throws AmplificationException {
-        final TestListener result = compileAndRunTests(classTest, currentTestList);
-        final long numberOfSubClasses = classTest.getFactory().Class().getAll().stream()
-                .filter(subClass -> classTest.getReference().equals(subClass.getSuperclass()))
-                .count();
-        if (!result.getFailingTests().isEmpty() ||
-                (!classTest.getModifiers().contains(ModifierKind.ABSTRACT) &&
-                        result.getRunningTests().size() != currentTestList.size()) ||
-                (classTest.getModifiers().contains(ModifierKind.ABSTRACT) &&
-                        result.getRunningTests().size() != (numberOfSubClasses * currentTestList.size()))) {
-            throw new AmplificationException(result.getFailingTests().size() +
-                    "/ " + result.getRunningTests().size() + " test cases failed!"
-                    + AmplificationHelper.LINE_SEPARATOR +
-                    result.getFailingTests()
-                            .stream()
-                            .map(Failure::toString)
-                            .collect(Collectors.joining(AmplificationHelper.LINE_SEPARATOR))
-            );
-        } else {
-            return result;
         }
     }
 }
