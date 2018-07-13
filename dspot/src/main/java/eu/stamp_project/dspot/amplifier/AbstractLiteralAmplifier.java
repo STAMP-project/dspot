2d7d852dbcf5c931686bb32883193034abe169e8
danglotb <bdanglot@gmail.com>
2018-07-13 17:33:18 +0200

    fix: do not amplify literals to avoid redundant amplifications

--- dspot/src/main/java/eu/stamp_project/dspot/amplifier/AbstractLiteralAmplifier.java
+++ dspot/src/main/java/eu/stamp_project/dspot/amplifier/AbstractLiteralAmplifier.java
@@ -3,10 +3,12 @@ package eu.stamp_project.dspot.amplifier;
 import eu.stamp_project.utils.AmplificationChecker;
 import eu.stamp_project.utils.AmplificationHelper;
 import spoon.reflect.code.CtAssignment;
+import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.visitor.filter.TypeFilter;
@@ -36,6 +38,7 @@ public abstract class AbstractLiteralAmplifier<T> implements Amplifier {
                 Class<?> clazzOfLiteral = null;
                 if ((literal.getParent() instanceof CtInvocation &&
                         AmplificationChecker.isAssert((CtInvocation) literal.getParent()))
+                        || isConcatenationOfLiteral(literal)
                         || literal.getParent(CtAnnotation.class) != null) {
                     return false;
                 } else if (literal.getValue() == null) {
@@ -65,6 +68,15 @@ public abstract class AbstractLiteralAmplifier<T> implements Amplifier {
                 // maybe need a warning ?
                 return false;
             }
+
+        }
+        private boolean isConcatenationOfLiteral(CtLiteral<T> literal) {
+            CtElement currentElement = literal;
+            while (currentElement.getParent() instanceof CtBinaryOperator) {
+                currentElement = currentElement.getParent();
+            }
+            return currentElement.getParent() instanceof CtInvocation &&
+                    AmplificationChecker.isAssert((CtInvocation) currentElement.getParent());
         }
     };
 
@@ -74,7 +86,18 @@ public abstract class AbstractLiteralAmplifier<T> implements Amplifier {
         if (literals.isEmpty()) {
             return Stream.empty();
         }
+        // we now reduce the literals list, see https://github.com/STAMP-project/dspot/issues/454
+        final Integer maxIndex = literals.stream()
+                .filter(this::hasBeenAmplified)
+                .map(literals::indexOf)
+                .max(Integer::compareTo)
+                .orElse(-1);
+        if (maxIndex > -1 && maxIndex <= literals.size()) {
+            literals = literals.subList(maxIndex + 1, literals.size());
+        }
         return literals.stream()
+                .filter(literal -> !this.hasBeenAmplified(literal))
+                // we now filter here, in order to keep marked elements, to reduce the list
                 .flatMap(literal -> this.amplify(literal).stream()
                         .map(newValue -> replace(literal, newValue, testMethod))
                 );
@@ -84,10 +107,21 @@ public abstract class AbstractLiteralAmplifier<T> implements Amplifier {
         final T originalValue = oldLiteral.getValue();
         oldLiteral.setValue(newValue);
         oldLiteral.putMetadata(METADATA_KEY, true);
+        oldLiteral.setDocComment(METADATA_KEY); // here, we use the DocComment since the Metadata are not cloned
         CtMethod<?> clone = AmplificationHelper.cloneTestMethodForAmp(testMethod, getSuffix());
         oldLiteral.setValue(originalValue);
+        oldLiteral.setDocComment("");
         oldLiteral.putMetadata(METADATA_KEY, false);
         return clone;
+    }
+
+
+    // checks rather than a literal has been amplified
+    private boolean hasBeenAmplified(CtLiteral<T> literal) {
+        return (literal.getMetadata(METADATA_KEY) != null &&
+                (boolean) literal.getMetadata(METADATA_KEY)) ||
+                (literal.getDocComment() != null &&
+                literal.getDocComment().equals(METADATA_KEY));
     }
 
     @Override
