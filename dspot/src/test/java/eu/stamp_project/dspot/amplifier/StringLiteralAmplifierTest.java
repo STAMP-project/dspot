6f250419f720b27fa428d59ffa5656cdf2365c71
danglotb <bdanglot@gmail.com>
2018-07-13 15:49:04 +0200

    test: do not amplify char

--- dspot/src/test/java/eu/stamp_project/dspot/amplifier/StringLiteralAmplifierTest.java
+++ dspot/src/test/java/eu/stamp_project/dspot/amplifier/StringLiteralAmplifierTest.java
@@ -29,43 +29,6 @@ public class StringLiteralAmplifierTest extends AbstractTest {
         final String nameMethod = "methodString";
         CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
         AmplificationHelper.setSeedRandom(42L);
-        Amplifier amplificator = new StringLiteralAmplifier();
-        amplificator.reset(literalMutationClass);
-        CtMethod method = literalMutationClass.getMethod(nameMethod);
-        List<CtMethod> mutantMethods = amplificator.apply(method).collect(Collectors.toList());
-        assertEquals(43, mutantMethods.size());
-        mutantMethods = amplificator.apply(method).collect(Collectors.toList());
-        assertEquals(23, mutantMethods.size());
-        amplificator.reset(literalMutationClass);
-        mutantMethods = amplificator.apply(method).collect(Collectors.toList());
-        assertEquals(43, mutantMethods.size());
-    }
-
-    @Test
-    public void testDoesNotAmplifyChar() throws Exception {
-        final String nameMethod = "methodCharacter";
-        CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
-        AmplificationHelper.setSeedRandom(42L);
-        Amplifier mutator = new StringLiteralAmplifier();
-        mutator.reset(literalMutationClass);
-        CtMethod method = literalMutationClass.getMethod(nameMethod);
-        List<CtMethod> mutantMethods = mutator.apply(method).collect(Collectors.toList());
-        assertTrue(mutantMethods.isEmpty());
-    }
-
-    @Test
-    public void testChainAmplify() throws Exception {
-
-        /*
-            test the StringLiteral
-            The first iteration is complete, i.e. apply random operations plus the specific strings
-            the second iteration does not apply the specific strings, we already did
-            After the reset, the amplifier should obtain the same number than a the first iteration.
-         */
-
-        final String nameMethod = "methodString";
-        CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
-        AmplificationHelper.setSeedRandom(42L);
         Amplifier amplifier = new StringLiteralAmplifier();
         amplifier.reset(literalMutationClass);
         CtMethod method = literalMutationClass.getMethod(nameMethod);
@@ -79,6 +42,18 @@ public class StringLiteralAmplifierTest extends AbstractTest {
         amplifier.reset(literalMutationClass);
         mutantMethods = amplifier.apply(method);
         assertEquals(28, mutantMethods.count());
+    }
+
+    @Test
+    public void testDoesNotAmplifyChar() throws Exception {
+        final String nameMethod = "methodCharacter";
+        CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
+        AmplificationHelper.setSeedRandom(42L);
+        Amplifier mutator = new StringLiteralAmplifier();
+        mutator.reset(literalMutationClass);
+        CtMethod method = literalMutationClass.getMethod(nameMethod);
+        List<CtMethod> mutantMethods = mutator.apply(method).collect(Collectors.toList());
+        assertTrue(mutantMethods.isEmpty());
     }
 
     @Test
