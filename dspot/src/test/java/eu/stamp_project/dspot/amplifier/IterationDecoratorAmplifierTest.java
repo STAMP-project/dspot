5dc13be4d717aaf9ead8e4a9d8107689126e0927
danglotb <bdanglot@gmail.com>
2018-07-16 23:21:12 +0200

    test: the IterationDecorator should apply the decorated amplifier at specific iteration

---
+++ dspot/src/test/java/eu/stamp_project/dspot/amplifier/IterationDecoratorAmplifierTest.java
@@ -0,0 +1,48 @@
+package eu.stamp_project.dspot.amplifier;
+
+import eu.stamp_project.AbstractTest;
+import eu.stamp_project.Utils;
+import eu.stamp_project.utils.AmplificationHelper;
+import org.junit.Test;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 16/07/18
+ */
+public class IterationDecoratorAmplifierTest extends AbstractTest {
+
+    @Test
+    public void test() throws Exception {
+
+        /*
+            Test the application of IterationDecoratorAmplifier.
+            This amplifier decorate the given amplifier to apply it at a given frequency
+         */
+
+        final String nameMethod = "methodInteger";
+        CtClass<Object> literalMutationClass = Utils.getFactory().Class().get("fr.inria.amp.LiteralMutation");
+        AmplificationHelper.setSeedRandom(42L);
+        // we apply the given amplifier each 2 iteration, i.e. iteration 0, iteration 2, iteration 4 etc....
+        Amplifier amplifier = new IterationDecoratorAmplifier(new NumberLiteralAmplifier(), 2);
+        CtMethod method = literalMutationClass.getMethod(nameMethod);
+
+        for (int i = 0 ; i < 10 ; i++) {
+            assertTrue(amplifier.apply(method).count() > 0);
+            assertTrue(amplifier.apply(method).count() > 0);
+        }
+
+        amplifier = new IterationDecoratorAmplifier(new NumberLiteralAmplifier(), 3);
+        for (int i = 0 ; i < 12 ; i++) {
+            assertFalse(amplifier.apply(method).count() > 0);
+            assertTrue(amplifier.apply(method).count() > 0);
+            assertTrue(amplifier.apply(method).count() > 0);
+        }
+
+    }
+}
