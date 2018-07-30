58b16084777d773e7a2ecb8e7cfd5fbdcee34ad1
danglotb <bdanglot@gmail.com>
2018-07-30 15:13:20 +0200

    test: update tests

--- dspot/src/test/java/eu/stamp_project/mutant/descartes/DescartesTest.java
+++ dspot/src/test/java/eu/stamp_project/mutant/descartes/DescartesTest.java
@@ -54,10 +54,10 @@ public class DescartesTest extends AbstractTest {
 
     @Test
     public void testInjectionOfDescartesIfNeeded() throws Exception {
-        assertTrue(DescartesChecker.shouldInjectDescartes());
+        assertTrue(DescartesChecker.shouldInjectDescartes(pathname));
         Utils.getInputConfiguration().setPitVersion("1.4.0");
-        DescartesInjector.injectDescartesIntoPom(Utils.getInputConfiguration(), pathname);
-        assertFalse(DescartesChecker.shouldInjectDescartes());
+        DescartesInjector.injectDescartesIntoPom(pathname);
+        assertFalse(DescartesChecker.shouldInjectDescartes(pathname));
         try (BufferedReader buffer = new BufferedReader(new FileReader(pathname))) {
             final String pomAsStr = buffer.lines().collect(Collectors.joining(AmplificationHelper.LINE_SEPARATOR));
             System.out.println(pomAsStr);
