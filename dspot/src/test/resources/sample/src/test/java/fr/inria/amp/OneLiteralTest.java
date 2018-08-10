b02a2502cb1483c107bb8dcfab6fef46dc950c72
danglotb <bdanglot@gmail.com>
2018-08-10 20:04:44 +0200

    fix: using existing literals if at least two elements exist

---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/amp/OneLiteralTest.java
@@ -0,0 +1,16 @@
+package fr.inria.amp;
+
+import org.junit.Test;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 10/08/18
+ */
+public class OneLiteralTest {
+
+    @Test
+    public void test() throws Exception {
+        String oneLit = "";
+    }
+}
