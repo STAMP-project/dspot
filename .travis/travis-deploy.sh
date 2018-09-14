6453ed98f64a615c21240b3a4052a30814d535f0
danglotb <benjamin.danglot@inria.fr>
2018-09-14 11:45:50 +0200

    fix: launch checkstyle in a dedicated node of travis and skip it when deploy

--- .travis/travis-deploy.sh
+++ .travis/travis-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-    cp .travis/travis-settings.xml $HOME/.m2/settings.xml && mvn deploy -DskipTests
+    cp .travis/travis-settings.xml $HOME/.m2/settings.xml && mvn deploy -DskipTests -Dcheckstyle.skip
 else
     echo "Nothing to deploy when on PR or other branch"
 fi
