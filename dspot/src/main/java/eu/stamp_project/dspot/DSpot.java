ec7114f0327c442b07a7ac5a7634c9a986f7be45
danglotb <benjamin.danglot@inria.fr>
2018-10-24 09:30:36 +0200

    style: remove unused import

--- dspot/src/main/java/eu/stamp_project/dspot/DSpot.java
+++ dspot/src/main/java/eu/stamp_project/dspot/DSpot.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
