dd3d0af193c12cb0ac7969af33565b77a62cd185
danglotb <benjamin.danglot@inria.fr>
2018-10-29 17:24:13 +0100

    test: add a test for the recovery from errors from multiple point

---
+++ dspot/src/test/java/eu/stamp_project/dspot/RecoveryDSpotTest.java
@@ -0,0 +1,116 @@
+package eu.stamp_project.dspot;
+
+import eu.stamp_project.AbstractTest;
+import eu.stamp_project.Utils;
+import eu.stamp_project.dspot.amplifier.Amplifier;
+import eu.stamp_project.dspot.assertgenerator.AssertGenerator;
+import eu.stamp_project.dspot.selector.PitMutantScoreSelector;
+import eu.stamp_project.dspot.selector.TakeAllSelector;
+import eu.stamp_project.program.InputConfiguration;
+import eu.stamp_project.utils.compilation.DSpotCompiler;
+import org.junit.After;
+import org.junit.Test;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 29/10/18
+ */
+public class RecoveryDSpotTest extends AbstractTest {
+
+    @After
+    public void tearDown() throws Exception {
+        InputConfiguration.get().setSelector(new PitMutantScoreSelector());
+    }
+
+    public class SelectorThatThrowsError extends TakeAllSelector {
+
+        private boolean throwsToAmplify;
+
+        private boolean throwsToKeep;
+
+        public void setThrowsToAmplify(boolean throwsToAmplify) {
+            this.throwsToAmplify = throwsToAmplify;
+        }
+
+        public void setThrowsToKeep(boolean throwsToKeep) {
+            this.throwsToKeep = throwsToKeep;
+        }
+
+        @Override
+        public List<CtMethod<?>> selectToAmplify(CtType<?> classTest, List<CtMethod<?>> testsToBeAmplified) {
+            if (throwsToAmplify) {
+                throw new RuntimeException();
+            }
+            return super.selectToAmplify(classTest, testsToBeAmplified);
+        }
+
+        @Override
+        public List<CtMethod<?>> selectToKeep(List<CtMethod<?>> amplifiedTestToBeKept) {
+            if (throwsToKeep) {
+                throw new RuntimeException();
+            }
+            return super.selectToKeep(amplifiedTestToBeKept);
+        }
+    }
+
+    public class AmplifierThatThrowsError implements Amplifier {
+        @Override
+        public Stream<CtMethod<?>> amplify(CtMethod<?> testMethod, int iteration) {
+            throw new RuntimeException();
+        }
+
+        @Override
+        public void reset(CtType<?> testClass) {
+
+        }
+    }
+
+    public class AssertGeneratorThatThrowsError extends AssertGenerator {
+        public AssertGeneratorThatThrowsError(DSpotCompiler compiler) {
+            super(InputConfiguration.get(), compiler);
+        }
+
+        @Override
+        public List<CtMethod<?>> assertionAmplification(CtType<?> testClass, List<CtMethod<?>> tests) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Test
+    public void testThatDSpotCanRecoverFromError() throws Exception {
+
+        /*
+            We test here, with different mock, that DSpot can recover for errors, continue and terminate the amplification process.
+         */
+
+        final SelectorThatThrowsError selector = new SelectorThatThrowsError();
+        InputConfiguration.get().setSelector(selector);
+        selector.setThrowsToAmplify(true);
+        Amplification amplification = new Amplification(Utils.getCompiler());
+        amplification.amplification(Utils.findClass("fr.inria.amp.OneLiteralTest"), 1);
+
+        selector.setThrowsToAmplify(false);
+        selector.setThrowsToKeep(true);
+        amplification.amplification(Utils.findClass("fr.inria.amp.OneLiteralTest"), 1);
+
+        InputConfiguration.get().setSelector(new TakeAllSelector());
+        InputConfiguration.get().setAmplifiers(Collections.singletonList(new AmplifierThatThrowsError()));
+        amplification.amplification(Utils.findClass("fr.inria.amp.OneLiteralTest"), 1);
+
+        InputConfiguration.get().setAmplifiers(Collections.emptyList());
+        amplification.amplification(Utils.findClass("fr.inria.amp.OneLiteralTest"), 1);
+
+        final Field assertGenerator = amplification.getClass().getDeclaredField("assertGenerator");
+        assertGenerator.setAccessible(true);
+        assertGenerator.set(amplification, new AssertGeneratorThatThrowsError(Utils.getCompiler()));
+        amplification.amplification(Utils.findClass("fr.inria.amp.OneLiteralTest"), 1);
+    }
+}
