97f3e2d0dbbb0cc13ee6d553c65fc794fa20a773
danglotb <benjamin.danglot@inria.fr>
2018-11-05 13:47:59 +0100

    ci: hotfix for the ci. We use now the dspot version computed by travis rather than hardcoded one

--- .travis/travis-dhell.sh
+++ .travis/travis-dhell.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+DPSOT_VERSION=${1}
+
 cd dspot
 git clone https://github.com/STAMP-project/dhell.git
 cd dhell
 
-java -jar ../target/dspot-1.2.1-SNAPSHOT-jar-with-dependencies.jar --path-to-properties dhell.dspot --iteration 1 --amplifiers MethodAdd --test eu.stamp_project.examples.dhell.HelloAppTest
+java -jar ../target/dspot-${DSPOT_VERSION}-jar-with-dependencies.jar --path-to-properties dhell.dspot --iteration 1 --amplifiers MethodAdd --test eu.stamp_project.examples.dhell.HelloAppTest
 
 if [ -f dspot-out/eu/stamp_project/examples/dhell/HelloAppTest.java ]; then
     exit 0
