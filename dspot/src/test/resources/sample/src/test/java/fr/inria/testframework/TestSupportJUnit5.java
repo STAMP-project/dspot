52ca66122867da2736053c65fc17e3c6a033ca84
danglotb <benjamin.danglot@inria.fr>
2018-11-07 15:56:38 +0100

    test: begin the test suite on the support of different test framework

---
+++ dspot/src/test/resources/sample/src/test/java/fr/inria/testframework/TestSupportJUnit5.java
@@ -0,0 +1,18 @@
+package fr.inria.testframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 07/11/18
+ */
+public class TestSupportJUnit5 {
+
+    @Test
+    public void testJUnit3() {
+        assertTrue(true);
+    }
+}
