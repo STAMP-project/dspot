bac4b1b758250b0382059084df49dbe31cb47285
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-11-19 15:31:55 +0100

    hotfix: make the installation of XWiki silent

--- .travis/travis-ci-xwiki.sh
+++ .travis/travis-ci-xwiki.sh
@@ -6,7 +6,7 @@ cd dspot
 git clone https://github.com/xwiki/xwiki-commons.git
 cd xwiki-commons
 git reset --hard fad7990ee27aa6de039412587fc9546b90845e34
-mvn clean install -DskipTests
+mvn clean install -DskipTests --quiet
 cd ..
 
 pwd
