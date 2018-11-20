105708e213faf9cbad70d8a8ea9aae5bf8114724
danglotb <benjamin.danglot@inria.fr>
2018-11-20 18:49:41 +0100

    ci: fix command line

--- .travis/travis-dspot-maven-cmd-line.sh
+++ .travis/travis-dspot-maven-cmd-line.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DPSOT_VERSION=${1}
+DSPOT_VERSION=${1}
 
 cd dspot-maven/src/test/resources/multi-module
-mvn eu.stamp-project:dspot-maven:1.2.2-SNAPSHOT:amplify-unit-tests -Dverbose -Dpath-to-properties=multi-module.properties -Damplifiers=TestDataMutator -Diteration=1 -Dtest=example.TestSuiteExample -Dtest-criterion=JacocoCoverageSelector
+mvn eu.stamp-project:dspot-maven:${DSPOT_VERSION}:amplify-unit-tests -Dverbose -Dpath-to-properties=multi-module.properties -Damplifiers=TestDataMutator -Diteration=1 -Dtest=example.TestSuiteExample -Dtest-criterion=JacocoCoverageSelector
 
 if [ -f target/dspot/output/example/TestSuiteExample.java ]; then
     exit 0
