28082f339d001ec6738c3fbb863d87dbe4ca412f
danglotb <benjamin.danglot@inria.fr>
2018-11-23 14:36:40 +0100

    test: add some test on check version

--- dspot/src/test/java/eu/stamp_project/utils/check/CheckerTest.java
+++ dspot/src/test/java/eu/stamp_project/utils/check/CheckerTest.java
@@ -1,10 +1,12 @@
 package eu.stamp_project.utils.check;
 
 import eu.stamp_project.Main;
+import eu.stamp_project.utils.options.check.Checker;
 import eu.stamp_project.utils.options.check.InputErrorException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -13,6 +15,48 @@ import static org.junit.Assert.fail;
  * on 22/11/18
  */
 public class CheckerTest {
+
+    @Test
+    public void testCheckVersion() {
+        Checker.checkIsACorrectVersion("1");
+        Checker.checkIsACorrectVersion("10");
+        Checker.checkIsACorrectVersion("10.1");
+        Checker.checkIsACorrectVersion("10.10");
+
+        Checker.checkIsACorrectVersion("1.1.1");
+        Checker.checkIsACorrectVersion("1.1.10");
+        Checker.checkIsACorrectVersion("1.10.10");
+        Checker.checkIsACorrectVersion("10.1.10");
+        Checker.checkIsACorrectVersion("10.10.10");
+
+        try {
+            Checker.checkIsACorrectVersion("1.");
+            fail("should have thrown InputErrorException");
+        } catch (InputErrorException e) {
+            // expected
+        }
+
+        try {
+            Checker.checkIsACorrectVersion("a.");
+            fail("should have thrown InputErrorException");
+        } catch (InputErrorException e) {
+            // expected
+        }
+
+        try {
+            Checker.checkIsACorrectVersion("1.a");
+            fail("should have thrown InputErrorException");
+        } catch (InputErrorException e) {
+            // expected
+        }
+
+        try {
+            Checker.checkIsACorrectVersion("b");
+            fail("should have thrown InputErrorException");
+        } catch (InputErrorException e) {
+            // expected
+        }
+    }
 
     @Test
     public void testWrongPathToProperties() {
