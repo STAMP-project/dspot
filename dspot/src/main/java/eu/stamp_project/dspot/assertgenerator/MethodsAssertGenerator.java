f6c66f27d3f19543ad35b07ac148738405b9ab2c
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-02-11 12:50:06 +0100

    DSpot optimization using caching of Test Framework associated to cloned test methods

--- dspot/src/main/java/eu/stamp_project/dspot/assertgenerator/MethodsAssertGenerator.java
+++ dspot/src/main/java/eu/stamp_project/dspot/assertgenerator/MethodsAssertGenerator.java
@@ -97,7 +97,9 @@ public class MethodsAssertGenerator {
         final List<CtMethod<?>> testsToRun = new ArrayList<>();
         IntStream.range(0, 3).forEach(i -> testsToRun.addAll(
                 testCasesWithLogs.stream()
-                        .map(CtMethod::clone)
+			//Optimization: Tracking cloned test methods using AmplificationHelper as candidates 
+                	//for caching their associated Test Framework
+                        .map(CloneHelper::cloneMethod) 
                         .peek(ctMethod -> ctMethod.setSimpleName(ctMethod.getSimpleName() + i))
                         .peek(clone::addMethod)
                         .collect(Collectors.toList())
