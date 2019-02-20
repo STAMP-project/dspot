47cf995deb7a808c9f2144c45538e708633ea6c9
danglotb <benjamin.danglot@inria.fr>
2019-02-20 11:30:42 +0100

    ci: enable all jobs

--- .travis/travis-coverage.sh
+++ .travis/travis-coverage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd dspot && mvn -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID clean test -Dtest=eu.stamp_project.dspot.amplifier.CharacterLiteralAmplifierTest jacoco:report coveralls:report -DrepoToken=${2}
+cd dspot && mvn -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID clean test jacoco:report coveralls:report
