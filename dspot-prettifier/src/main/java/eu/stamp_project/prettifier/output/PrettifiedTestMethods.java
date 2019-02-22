ee7ffddc85406517edc18d54964b75d28b3d9c6f
danglotb <benjamin.danglot@inria.fr>
2019-02-22 19:44:16 +0100

    feat: add output package

---
+++ dspot-prettifier/src/main/java/eu/stamp_project/prettifier/output/PrettifiedTestMethods.java
@@ -0,0 +1,36 @@
+package eu.stamp_project.prettifier.output;
+
+import eu.stamp_project.test_framework.TestFramework;
+import eu.stamp_project.utils.DSpotUtils;
+import eu.stamp_project.utils.program.InputConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 22/02/19
+ */
+public class PrettifiedTestMethods {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrettifiedTestMethods.class);
+
+    public static void output(
+            CtType<?> testClass,
+            List<CtMethod<?>> prettifiedAmplifiedTestMethods
+    ) {
+        final String outputDirectory = InputConfiguration.get().getOutputDirectory();
+        testClass.getMethods().stream()
+                .filter(TestFramework.get()::isTest)
+                .forEach(testClass::removeMethod);
+        prettifiedAmplifiedTestMethods.forEach(testClass::addMethod);
+        DSpotUtils.printCtTypeToGivenDirectory(testClass, new File(outputDirectory), true);
+        LOGGER.info("Print {} in {}", testClass.getQualifiedName(), outputDirectory);
+    }
+
+}
