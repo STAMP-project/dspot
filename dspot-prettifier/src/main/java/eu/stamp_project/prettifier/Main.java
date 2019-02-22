2f2a1b5a3e35ac7fedbc9fe88ee3620f345e3624
danglotb <benjamin.danglot@inria.fr>
2019-02-22 19:28:33 +0100

    feat: skeleton of the process

--- dspot-prettifier/src/main/java/eu/stamp_project/prettifier/Main.java
+++ dspot-prettifier/src/main/java/eu/stamp_project/prettifier/Main.java
@@ -3,14 +3,19 @@ package eu.stamp_project.prettifier;
 import eu.stamp_project.prettifier.code2vec.Code2VecExecutor;
 import eu.stamp_project.prettifier.code2vec.Code2VecParser;
 import eu.stamp_project.prettifier.code2vec.Code2VecWriter;
+import eu.stamp_project.prettifier.minimization.GeneralMinimizer;
 import eu.stamp_project.prettifier.options.InputConfiguration;
 import eu.stamp_project.prettifier.options.JSAPOptions;
+import eu.stamp_project.prettifier.output.PrettifiedTestMethods;
+import eu.stamp_project.test_framework.TestFramework;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spoon.Launcher;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtType;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * created by Benjamin DANGLOT
@@ -30,10 +35,43 @@ public class Main {
 
     public static void main(String[] args) {
         JSAPOptions.parse(args);
+        final CtType<?> amplifiedTestClass = loadAmplifiedTestClass();
+        final List<CtMethod<?>> prettifiedAmplifiedTestMethods = run(amplifiedTestClass);
+        // output now
+        PrettifiedTestMethods.output(amplifiedTestClass, prettifiedAmplifiedTestMethods);
+    }
+
+    public static CtType<?> loadAmplifiedTestClass() {
+        Launcher launcher = new Launcher();
+        launcher.getEnvironment().setNoClasspath(true);
+        launcher.addInputResource(InputConfiguration.get().getPathToAmplifiedTestClass());
+        launcher.buildModel();
+        return launcher.getFactory().Class().getAll().get(0);
+    }
+
+    public static List<CtMethod<?>> run(CtType<?> amplifiedTestClass) {
+        // 1
+        final List<CtMethod<?>> minimizedAmplifiedTestMethods = applyMinimization(
+                TestFramework.getAllTest(amplifiedTestClass)
+        );
+        // 2
+
+        // TODO
+
+        // 3
         applyCode2Vec(InputConfiguration.get().getPathToRootOfCode2Vec(),
                 InputConfiguration.get().getRelativePathToModelForCode2Vec(),
-                Collections.emptyList()
+                minimizedAmplifiedTestMethods
         );
+        return minimizedAmplifiedTestMethods;
+    }
+
+    public static List<CtMethod<?>> applyMinimization(List<CtMethod<?>> amplifiedTestMethodsToBeRenamed) {
+        final GeneralMinimizer generalMinimizer = new GeneralMinimizer();
+        return amplifiedTestMethodsToBeRenamed.stream()
+                .map(generalMinimizer::minimize)
+                .collect(Collectors.toList());
+
     }
 
     public static void applyCode2Vec(String pathToRootOfCode2Vec,
