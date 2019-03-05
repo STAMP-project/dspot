c79351ad3a4e3507917d30c862e78649cdd55793
danglotb <benjamin.danglot@inria.fr>
2019-03-05 14:05:25 +0100

    test: udpate test

--- dspot-prettifier/src/test/java/eu/stamp_project/prettifier/code2vec/Code2VecExecutorTest.java
+++ dspot-prettifier/src/test/java/eu/stamp_project/prettifier/code2vec/Code2VecExecutorTest.java
@@ -1,5 +1,7 @@
 package eu.stamp_project.prettifier.code2vec;
 
+import eu.stamp_project.prettifier.options.InputConfiguration;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -16,14 +18,17 @@ public class Code2VecExecutorTest {
         These test methods requires to have code2vec in src/test/resources
      */
 
+    @Before
+    public void setUp() throws Exception {
+        InputConfiguration.get().setTimeToWaitForCode2vecInMillis(3000);
+        InputConfiguration.get().setPathToRootOfCode2Vec("src/test/resources/code2vec/code2vec");
+        InputConfiguration.get().setRelativePathToModelForCode2Vec("../model/saved_model_iter20");
+    }
+
     @Ignore // DOES NOT WORK ON TRAVIS, CANNOT FIND python3 cmd
     @Test
-    public void test() {
-        final String pathToRootOfCode2Vec = "src/test/resources/code2vec/code2vec";
-        final Code2VecExecutor code2VecExecutor = new Code2VecExecutor(
-                pathToRootOfCode2Vec,
-                "../model/saved_model_iter20"
-        );
+    public void test() { ;
+        final Code2VecExecutor code2VecExecutor = new Code2VecExecutor();
         code2VecExecutor.run();
         final String output = code2VecExecutor.getOutput();
         System.out.println(output);
