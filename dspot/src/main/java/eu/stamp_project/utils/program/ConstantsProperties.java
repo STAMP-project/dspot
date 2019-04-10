9f6a71b1df73fed2e780a6f41e311b2601af6e87
danglotb <benjamin.danglot@inria.fr>
2019-04-10 13:33:11 +0200

    style: remove unused imports

--- dspot/src/main/java/eu/stamp_project/utils/program/ConstantsProperties.java
+++ dspot/src/main/java/eu/stamp_project/utils/program/ConstantsProperties.java
@@ -3,7 +3,6 @@ package eu.stamp_project.utils.program;
 import eu.stamp_project.utils.AmplificationHelper;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
