e03c67578ac0f66f6c0e85a400b22507b671e9b8
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2019-05-06 12:44:15 +0200

    Feat: do not generate assertion on string that contains path (#782)
    * test: add a test for some specific values
    
    * feat: do not add assertion on strings that contain path

--- dspot/src/main/java/eu/stamp_project/test_framework/implementations/junit/JUnitSupport.java
+++ dspot/src/main/java/eu/stamp_project/test_framework/implementations/junit/JUnitSupport.java
@@ -166,7 +166,8 @@ public abstract class JUnitSupport extends AbstractTestFramework {
                 factory.createVariableRead(parameter.getReference(), false),
                 factory.Class().get(java.lang.Throwable.class).getMethodsByName("getMessage").get(0).getReference()
         );
-        if (!AssertGeneratorHelper.containsObjectReferences(failure.messageOfFailure)) {
+        if (!AssertGeneratorHelper.containsObjectReferences(failure.messageOfFailure)
+                || !AssertGeneratorHelper.containsAPath(failure.messageOfFailure)) {
             ctCatch.getBody().addStatement(
                     this.buildInvocationToAssertion(
                             testMethod,
