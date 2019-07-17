2a297c9652e823fece7a42045d3a2fb717eda473
Jesús Gorroñogoitia Cruz <jesus.gorronogoitia@atos.net>
2019-07-17 11:41:44 +0200

    Fix: detection(filtering out) of @Ignored (JUnit4) or @Disabled (JUnit5) test suites before proceeding with amplification (#835)
    * Fix: detection(filtering out) of @Ignored (JUnit4) or @Disabled (JUnit5) test suites before proceeding with amplification
    
    * Fix: accommodating Test Suite Ignore/Disable detection to TestFrameworkSupport

--- dspot/src/main/java/eu/stamp_project/dspot/DSpot.java
+++ dspot/src/main/java/eu/stamp_project/dspot/DSpot.java
@@ -250,9 +250,17 @@ public class DSpot {
         final List<CtType<?>> testClassesToBeAmplifiedModel = testClassesToBeAmplified.stream()
                 .flatMap(this::findTestClasses)
                 .collect(Collectors.toList());
+        //Filtering out testClasses tagged as ignored (JUnit4) or disable (JUnit 5)
         return testClassesToBeAmplifiedModel.stream()
-                .map(ctType ->
-                        this._amplify(ctType, this.buildListOfTestMethodsToBeAmplified(ctType, testMethods))
+                .filter(ctType-> {
+                    boolean isIgnored = TestFramework.get().isIgnored(ctType);
+                    if (isIgnored) {
+                        LOGGER.info("Skiping test suite {} since it is annotated as ignored/disabled", ctType.getSimpleName());
+                    }
+                    return !isIgnored;
+                }
+                ).map(ctType ->
+                    this._amplify(ctType, this.buildListOfTestMethodsToBeAmplified(ctType, testMethods))
                 ).collect(Collectors.toList());
     }
 
