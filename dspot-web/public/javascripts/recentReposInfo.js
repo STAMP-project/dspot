4bb195d269f6e346648ad87a0bafc57ce89d7e22
tailp <tailp@kth.se>
2019-08-05 15:57:30 +0200

    Fix: hide UI selector(always Pitmutant) and branch(always master) choice

--- dspot-web/public/javascripts/recentReposInfo.js
+++ dspot-web/public/javascripts/recentReposInfo.js
@@ -10,6 +10,9 @@ formApp.controller("formCtr", function($scope, $http) {
     }
 
     $scope.submit = function() {
+        /*Default branch name as master - removed in later version*/
+        document.getElementById("gitBranch").defaultValue = "master";
+
         $scope.successAlert = false;
         $scope.failedAlert = false;
         var dataToSubmit = angular.copy($scope.user);
