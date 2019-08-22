41b0e994a4c0ae04339284eecc40680645dd5c3a
tailp <tailp@kth.se>
2019-08-23 00:16:42 +0200

    Feat: add more JSAPOptions , restful mode for web + test

--- dspot/src/main/java/eu/stamp_project/utils/collector/CollectorConfig.java
+++ dspot/src/main/java/eu/stamp_project/utils/collector/CollectorConfig.java
@@ -7,15 +7,15 @@ import org.slf4j.LoggerFactory;
 // Used to collect output information from selector class.
 public class CollectorConfig {
 
-	/* Initialization */
-	private static CollectorConfig collectorConfig;
+    /* Initialization */
+    private static CollectorConfig collectorConfig;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CollectorConfig.class);
 
     public static CollectorConfig getInstance() {
-    	if (collectorConfig == null ) {
-    		collectorConfig = new CollectorConfig();
-    	}
+        if (collectorConfig == null ) {
+            collectorConfig = new CollectorConfig();
+        }
         return collectorConfig;
     }
 
@@ -32,6 +32,11 @@ public class CollectorConfig {
 
     /* MongodbCollector related */
     private static String mongoUrl;
+    private static String mongoDbname;
+    private static String mongoColname;
+    private static String repoSlug;
+    private static String repoBranch;
+    private static boolean restful;
 
     public void setMongoUrl(String mongoUrl) {
         this.mongoUrl = mongoUrl;
@@ -39,5 +44,45 @@ public class CollectorConfig {
 
     public String getMongoUrl() {
         return this.mongoUrl;
+    }
+
+    public void setMongoDbname(String mongoDbname) {
+        this.mongoDbname = mongoDbname;
+    }
+
+    public String getMongoDbname() {
+        return this.mongoDbname;
+    }
+
+    public void setMongoColname(String mongoColname) {
+        this.mongoColname = mongoColname;
+    }
+
+    public String getMongoColname() {
+        return this.mongoColname;
+    }
+
+    public void setRepoSlug(String repoSlug) {
+        this.repoSlug = repoSlug;
+    }
+
+    public String getRepoSlug() {
+        return this.repoSlug;
+    }
+
+    public void setRepoBranch(String repoBranch) {
+        this.repoBranch = repoBranch;
+    }
+
+    public String getRepoBranch() {
+        return this.repoBranch;
+    }
+
+    public void setRestful(boolean restful) {
+        this.restful = restful;
+    }
+
+    public boolean getRestful() {
+        return this.restful;
     }
 }
--- dspot/src/test/java/eu/stamp_project/utils/collector/mongodb/MongodbCollectorTest.java
+++ dspot/src/test/java/eu/stamp_project/utils/collector/mongodb/MongodbCollectorTest.java
@@ -22,9 +22,9 @@ import org.bson.Document;
 
 public class MongodbCollectorTest {
 
-	@Test
-	public void testInfoSubmissionToMongodbPitMutantScoreSelector() {
-		Main.main(new String[]{
+        @Test
+        public void testInfoSubmissionToMongodbPitMutantScoreSelector() {
+                Main.main(new String[]{
                         "--path-to-properties", "src/test/resources/sample/sample.properties",
                         "--test-criterion", "PitMutantScoreSelector",
                         "--test", "fr.inria.sample.TestClassWithoutAssert",
@@ -32,44 +32,92 @@ public class MongodbCollectorTest {
                         "--gregor",
                         "--output-path", "target/trash",
                         "--collector","MongodbCollector",
-                        "--mongo-url","mongodb://localhost:27017"
+                        "--mongo-url","mongodb://localhost:27017",
+                        "--mongo-colname","AmpTestRecords",
+                        "--mongo-dbname","Dspot",
+                        "--repo-slug","USER/Testing",
+                        "--repo-branch","master"
                 });
 
-        	MongoClient mongoClient = MongodbCollector.connectToMongo("mongodb://localhost:27017");
-        	MongoCollection<Document> coll = MongodbCollector.getCollection("AmpTestRecords",MongodbCollector.getDatabase("Dspot",mongoClient));
+                MongoClient mongoClient = MongodbCollector.connectToMongo("mongodb://localhost:27017");
+                MongoCollection<Document> coll = MongodbCollector.getCollection("AmpTestRecords",MongodbCollector.getDatabase("Dspot",mongoClient));
 
-        	Document foundDoc = coll.find(eq("RepoSlug","USER/Testing")).projection(fields(excludeId(),exclude("Date"),exclude("executeTestParallelWithNumberProcessors"))).first();
-        	coll.deleteOne(foundDoc);
+                Document foundDoc = coll.find(eq("RepoSlug","USER/Testing")).projection(fields(excludeId(),exclude("Date"),exclude("executeTestParallelWithNumberProcessors"))).first();
+                coll.deleteOne(foundDoc);
 
-        	Document unwanted = foundDoc.get("AmpOptions",Document.class);
-        	unwanted.remove("executeTestParallelWithNumberProcessors");
-        	foundDoc.append("AmpOptions",unwanted);
+                Document unwanted = foundDoc.get("AmpOptions",Document.class);
+                unwanted.remove("executeTestParallelWithNumberProcessors");
+                foundDoc.append("AmpOptions",unwanted);
 
-        	String expectedDocStr = "Document{{RepoSlug=USER/Testing, RepoBranch=master, AmpOptions=Document{{amplifiers=[None], test-criterion=PitMutantScoreSelector, iteration=3, gregor=true, descartes=true}}, AmpResult=Document{{fr/D/inria/D/sample/D/TestClassWithoutAssert=Document{{originalKilledMutants=0, NewMutantKilled=67}}, TotalResult=Document{{totalOriginalKilledMutants=0, totalNewMutantKilled=67}}}}}}";
+                String expectedDocStr = "Document{{RepoSlug=USER/Testing, RepoBranch=master, AmpOptions=Document{{amplifiers=[None], test-criterion=PitMutantScoreSelector, iteration=3, gregor=true, descartes=true}}, AmpResult=Document{{fr/D/inria/D/sample/D/TestClassWithoutAssert=Document{{originalKilledMutants=0, NewMutantKilled=67}}, TotalResult=Document{{totalOriginalKilledMutants=0, totalNewMutantKilled=67}}}}}}";
 
                 assertEquals(foundDoc.toString(),expectedDocStr);
-	}
+        }
 
-	@Test
-	public void testInfoSubmissionToMongodbJacocoCoverageSelector() {
+        @Test
+        public void testInfoSubmissionToMongodbJacocoCoverageSelector() {
                 Main.main(new String[]{
                         "--path-to-properties", "src/test/resources/project-with-resources/project-with-resources.properties",
                         "--test-criterion", "JacocoCoverageSelector",
                         "--iteration", "1",
                         "--collector","MongodbCollector",
-                        "--mongo-url","mongodb://localhost:27017"
+                        "--mongo-url","mongodb://localhost:27017",
+                        "--mongo-colname","AmpTestRecords",
+                        "--mongo-dbname","Dspot",
+                        "--repo-slug","USER/Testing",
+                        "--repo-branch","master"
                 });
                 MongoClient mongoClient = MongodbCollector.connectToMongo("mongodb://localhost:27017");
-        	MongoCollection<Document> coll = MongodbCollector.getCollection("AmpTestRecords",MongodbCollector.getDatabase("Dspot",mongoClient));
+                MongoCollection<Document> coll = MongodbCollector.getCollection("AmpTestRecords",MongodbCollector.getDatabase("Dspot",mongoClient));
 
-        	Document foundDoc = coll.find(eq("RepoSlug","USER/Testing")).projection(fields(excludeId(),exclude("Date"),exclude("executeTestParallelWithNumberProcessors"))).first();
-        	coll.deleteOne(foundDoc);
-        	Document unwanted = foundDoc.get("AmpOptions",Document.class);
-        	unwanted.remove("executeTestParallelWithNumberProcessors");
-        	foundDoc.append("AmpOptions",unwanted);
+                Document foundDoc = coll.find(eq("RepoSlug","USER/Testing")).projection(fields(excludeId(),exclude("Date"),exclude("executeTestParallelWithNumberProcessors"))).first();
+                coll.deleteOne(foundDoc);
+                Document unwanted = foundDoc.get("AmpOptions",Document.class);
+                unwanted.remove("executeTestParallelWithNumberProcessors");
+                foundDoc.append("AmpOptions",unwanted);
 
                 String expectedDocStr = "Document{{RepoSlug=USER/Testing, RepoBranch=master, AmpOptions=Document{{amplifiers=[None], test-criterion=JacocoCoverageSelector, iteration=1, gregor=false, descartes=true}}, AmpResult=Document{{resolver/D/ClasspathResolverTest=Document{{totalCoverage=130, initialCoverage=123, ampCoverage=123}}, textresources/D/in/D/sources/D/TestResourcesInSources=Document{{totalCoverage=130, initialCoverage=4, ampCoverage=4}}, TotalResult=Document{{totalCovAcrossAllTests=260, totalInitialCoverage=127, totalAmpCoverage=127}}}}}}";
 
-		assertEquals(foundDoc.toString(),expectedDocStr);
-	}
+                assertEquals(foundDoc.toString(),expectedDocStr);
+        }
+
+        @Test
+        public void testRestful() {
+                Document initDoc = new Document("RepoSlug", "USER/Testing")
+                                .append("RepoBranch", "master")
+                                .append("State","pending")
+                                .append("Email","abc@mail.com");
+                Document argsDoc = new Document();
+                argsDoc.append("test-criterion","PitMutantScoreSelector");
+                initDoc.append("AmpOptions",argsDoc);
+
+                MongoClient mongoClient = MongodbCollector.connectToMongo("mongodb://localhost:27017");
+                MongoCollection<Document> coll = MongodbCollector.getCollection("AmpTestRecords",MongodbCollector.getDatabase("Dspot",mongoClient));
+                coll.insertOne(initDoc);
+
+                Main.main(new String[]{
+                        "--path-to-properties", "src/test/resources/sample/sample.properties",
+                        "--test-criterion", "PitMutantScoreSelector",
+                        "--test", "fr.inria.sample.TestClassWithoutAssert",
+                        "--path-pit-result", "src/test/resources/sample/mutations.csv",
+                        "--gregor",
+                        "--output-path", "target/trash",
+                        "--collector","MongodbCollector",
+                        "--mongo-url","mongodb://localhost:27017",
+                        "--mongo-colname","AmpTestRecords",
+                        "--mongo-dbname","Dspot",
+                        "--repo-slug","USER/Testing",
+                        "--repo-branch","master",
+                        "--restful"
+                });
+
+                Document foundDoc = coll.find(eq("State","recent")).projection(fields(excludeId(),exclude("Date"),exclude("executeTestParallelWithNumberProcessors"))).first();
+                coll.deleteOne(foundDoc);
+                Document unwanted = foundDoc.get("AmpOptions",Document.class);
+                unwanted.remove("executeTestParallelWithNumberProcessors");
+                foundDoc.append("AmpOptions",unwanted);
+
+                String expectedDocStr = "Document{{RepoSlug=USER/Testing, RepoBranch=master, State=recent, Email=abc@mail.com, AmpOptions=Document{{amplifiers=[None], test-criterion=PitMutantScoreSelector, iteration=3, gregor=true, descartes=true}}, AmpResult=Document{{fr/D/inria/D/sample/D/TestClassWithoutAssert=Document{{originalKilledMutants=0, NewMutantKilled=67}}, TotalResult=Document{{totalOriginalKilledMutants=0, totalNewMutantKilled=67}}}}}}";
+                assertEquals(foundDoc.toString(),expectedDocStr);
+        }
 }
