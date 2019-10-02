b26fd1001ae3da41241e391716a900cdcf15de86
danglotb <benjamin.danglot@inria.fr>
2019-10-02 09:33:48 +0200

    ci: improve ci by using the amplified test

--- .travis/travis-ci-xwiki.sh
+++ .travis/travis-ci-xwiki.sh
@@ -11,8 +11,8 @@ cd ..
 
 mvn eu.stamp-project:dspot-maven:${DSPOT_VERSION}:amplify-unit-tests -Dgenerate-new-test-class=true -Dtest=org.xwiki.component.ProviderTest -Dtarget-module=xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default
 
-if [ -f target/dspot/output/org/xwiki/component/AmplProviderTest.java ]; then
-    exit 0
-else
-    exit 1
-fi
+cp target/dspot/output/org/xwiki/component/AmplProviderTest.java xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default/src/test/java/org/xwiki/component/
+
+cd xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default
+
+mvn clean test
