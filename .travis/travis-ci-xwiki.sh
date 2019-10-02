38db54b0f3b471d0df4e9dd59ca99adf707aa4bc
danglotb <benjamin.danglot@inria.fr>
2019-10-02 10:51:10 +0200

    ci: fix the script to amplify xwiki

--- .travis/travis-ci-xwiki.sh
+++ .travis/travis-ci-xwiki.sh
@@ -7,12 +7,11 @@ git clone https://github.com/xwiki/xwiki-commons.git
 cd xwiki-commons
 git reset --hard af3f542acc23e4d70cf61b4f6ac9d261a2a75bbf
 mvn clean install -DskipTests --quiet
-cd ..
-
 mvn eu.stamp-project:dspot-maven:${DSPOT_VERSION}:amplify-unit-tests -Dgenerate-new-test-class=true -Dtest=org.xwiki.component.ProviderTest -Dtarget-module=xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default
-
+if [not -f target/dspot/output/org/xwiki/component/AmplProviderTest.java ]; then
+    echo "Something went wrong during amplification"
+    exit 1
+else
 cp target/dspot/output/org/xwiki/component/AmplProviderTest.java xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default/src/test/java/org/xwiki/component/
-
 cd xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default
-
 mvn clean test
