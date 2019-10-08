5a71949cf27a171186ee17b18cc866c74fe41e22
danglotb <benjamin.danglot@inria.fr>
2019-10-08 22:18:10 +0200

    test: fix test of compilation package

--- dspot/src/test/java/eu/stamp_project/utils/compilation/TestCompilerTest.java
+++ dspot/src/test/java/eu/stamp_project/utils/compilation/TestCompilerTest.java
@@ -1,12 +1,13 @@
 package eu.stamp_project.utils.compilation;
 
-import eu.stamp_project.AbstractTest;
-import eu.stamp_project.Utils;
+import eu.stamp_project.dspot.AbstractTestOnSample;
+import eu.stamp_project.utils.DSpotUtils;
 import eu.stamp_project.utils.program.InputConfiguration;
 import org.junit.Test;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -16,32 +17,37 @@ import static org.junit.Assert.assertTrue;
  * benjamin.danglot@inria.fr
  * on 3/13/17
  */
-public class TestCompilerTest extends AbstractTest {
+public class TestCompilerTest extends AbstractTestOnSample {
 
     @Test
     public void test() throws Exception {
-        CtClass<?> testClass = Utils.findClass("fr.inria.filter.failing.FailingTest");
+        final InputConfiguration configuration = new InputConfiguration();
+        configuration.setAbsolutePathToProjectRoot(getPathToProjectRoot());
+        DSpotCompiler compiler = DSpotCompiler.createDSpotCompiler(configuration, "");
+        CtClass<?> testClass = findClass("fr.inria.filter.failing.FailingTest");
+        final ArrayList<CtMethod<?>> methods = new ArrayList<>();
+        methods.add(findMethod(testClass, "testAssertionError"));
+        methods.add(findMethod(testClass, "testFailingWithException"));
+        DSpotUtils.init(false, "target/dspot/",
+                configuration.getFullClassPathWithExtraDependencies(), getPathToProjectRoot()
+        );
+        TestCompiler.init(0, false, getPathToProjectRoot(), configuration.getClasspathClassesProject(), 10000);
         assertTrue(TestCompiler.compileRunAndDiscardUncompilableAndFailingTestMethods(
                 testClass,
-                Arrays.asList(
-                        Utils.findMethod(testClass, "testAssertionError"),
-                        Utils.findMethod(testClass, "testFailingWithException")
-                ),
-                Utils.getCompiler(),
-                InputConfiguration.get()
+                methods,
+                compiler
         ).isEmpty());
 
-        testClass = Utils.findClass("fr.inria.filter.passing.PassingTest");
+        testClass = findClass("fr.inria.filter.passing.PassingTest");
+        methods.clear();
+        methods.add(findMethod(testClass, "testAssertion"));
+        methods.add(findMethod(testClass, "testNPEExpected"));
+        methods.add(findMethod(testClass, "failingTestCase"));
         assertEquals(2,
                 TestCompiler.compileRunAndDiscardUncompilableAndFailingTestMethods(
                         testClass,
-                        Arrays.asList(
-                                Utils.findMethod(testClass, "testAssertion"),
-                                Utils.findMethod(testClass, "testNPEExpected"),
-                                Utils.findMethod(testClass, "failingTestCase")
-                        ),
-                        Utils.getCompiler(),
-                        InputConfiguration.get()
+                        methods,
+                        compiler
                 ).size()
         );
     }
