f76410e2991bfd04cd4609369037c83b4e425a9d
danglotb <benjamin.danglot@inria.fr>
2019-10-08 21:56:02 +0200

    test: make gradle test pass

--- dspot/src/test/java/eu/stamp_project/automaticbuilder/gradle/GradleAutomaticBuilderTest.java
+++ dspot/src/test/java/eu/stamp_project/automaticbuilder/gradle/GradleAutomaticBuilderTest.java
@@ -1,17 +1,18 @@
 package eu.stamp_project.automaticbuilder.gradle;
 
-import eu.stamp_project.Utils;
+import eu.stamp_project.Main;
 import eu.stamp_project.automaticbuilder.AutomaticBuilder;
-import eu.stamp_project.automaticbuilder.AutomaticBuilderFactory;
+import eu.stamp_project.utils.options.AutomaticBuilderEnum;
 import eu.stamp_project.utils.pit.AbstractPitResult;
 import eu.stamp_project.utils.pit.PitXMLResultParser;
 import eu.stamp_project.utils.program.InputConfiguration;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import spoon.reflect.declaration.CtClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.Launcher;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,49 +32,55 @@ import static org.junit.Assert.assertTrue;
  */
 public class GradleAutomaticBuilderTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GradleAutomaticBuilderTest.class);
+
     private AutomaticBuilder sut = null;
-    PitXMLResultParser parser;
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        Utils.reset();
-    }
+    private PitXMLResultParser parser;
 
-    private final String pathToConfFile = "src/test/resources/test-projects/test-projects.properties";
+    private final String pathToProjectRoot = "src/test/resources/test-projects/";
+
+    private Launcher launcher;
 
     @Before
     public void setUp() throws Exception {
+        Main.verbose = true;
         cleanTestEnv();
-        Utils.init(pathToConfFile);
-        Utils.LOGGER.debug("Test Set-up - Reading input parameters...");
-        InputConfiguration inputConfiguration = Utils.getInputConfiguration();
-        inputConfiguration.setBuilderName("GradleBuilder");
-        Utils.LOGGER.debug("Test Set-up - instantiating Automatic Builder (SUT)...");
-        sut = AutomaticBuilderFactory.getAutomaticBuilder(inputConfiguration.getBuilderName());
+        LOGGER.debug("Test Set-up - Reading input parameters...");
+        LOGGER.debug("Test Set-up - instantiating Automatic Builder (SUT)...");
+        final InputConfiguration configuration = new InputConfiguration();
+        configuration.setAbsolutePathToProjectRoot(this.pathToProjectRoot);
+        configuration.setDescartesMode(false);
+        configuration.setFilter("example.*");
+        launcher = new Launcher();
+        launcher.addInputResource(pathToProjectRoot + "src/");
+        launcher.getEnvironment().setNoClasspath(true);
+        launcher.buildModel();
+        sut = AutomaticBuilderEnum.Gradle.getAutomaticBuilder(configuration);
         sut.compile();
-        Utils.LOGGER.debug("Test Set-up complete.");
+        LOGGER.debug("Test Set-up complete.");
         parser = new PitXMLResultParser();
     }
 
     @After
     public void tearDown() throws Exception {
-        Utils.LOGGER.debug("Test Tear-down...");
+        LOGGER.debug("Test Tear-down...");
         cleanTestEnv();
-        Utils.LOGGER.debug("Test Tear-down complete.");
+        LOGGER.debug("Test Tear-down complete.");
     }
 
     @Test
     public void compile_whenCleanCompileTestTasksAreAppliedToDSpotTestExampleProject() throws Exception {
-        Utils.LOGGER.info("Starting Gradle Automatic Builder compile() test...");
+        LOGGER.info("Starting Gradle Automatic Builder compile() test...");
         sut.compile();
         File buildDir = new File("src/test/resources/test-projects/build");
         assertTrue("After compilation, build dir should exist", buildDir.exists());
-        Utils.LOGGER.info("Gradle Automatic Builder compile() test complete.");
+        LOGGER.info("Gradle Automatic Builder compile() test complete.");
     }
 
     @Test
     public void buildClasspath_whenAppliedToDSpotTestExampleProject() throws Exception {
-        Utils.LOGGER.info("Starting Gradle Automatic Builder buildClasspath() test...");
+        LOGGER.info("Starting Gradle Automatic Builder buildClasspath() test...");
         String[] expectedCompilationLibraries = {"log4j-api-2.8.2.jar", "log4j-core-2.8.2.jar"};
         String[] expectedTestCompilationLibraries = {"hamcrest-core-1.3.jar", "junit-4.12.jar"};
         String classPath = sut.buildClasspath();
@@ -85,63 +92,59 @@ public class GradleAutomaticBuilderTest {
         try (final BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/test-projects/" + GradleInjector.GRADLE_BUILD_FILE)))) {
             assertFalse( reader.lines().collect(Collectors.joining("\n")).contains(GradleInjector.WRITE_CLASSPATH_TASK));
         }
-        Utils.LOGGER.info("Gradle Automatic Builder buildClasspath() test complete.");
+        LOGGER.info("Gradle Automatic Builder buildClasspath() test complete.");
     }
 
     @Test
     public void runPit_whenNoTestClassIsSpecified() throws Exception {
-        Utils.LOGGER.info("Starting Gradle Automatic Builder runPit() test when no test class is specified...");
-        InputConfiguration.get().setDescartesMode(false);
+        LOGGER.info("Starting Gradle Automatic Builder runPit() test when no test class is specified...");
+        //InputConfiguration.get().setDescartesMode(false);
         sut.runPit();
         List<? extends AbstractPitResult> pitResults = parser.parseAndDelete("src/test/resources/test-projects/" + sut.getOutputDirectoryPit());
         assertTrue("PIT results shouldn't be null", pitResults != null);
         assertTrue("PIT results shouldn't be empty", !pitResults.isEmpty());
-        Utils.LOGGER.info("Gradle Automatic Builder runPit() test complete when no test class is specified.");
+        LOGGER.info("Gradle Automatic Builder runPit() test complete when no test class is specified.");
     }
 
     @Test
     public void runPit_whenTestClassIsSpecified() throws Exception {
-        Utils.LOGGER.info("Starting Gradle Automatic Builder runPit() test when a test class is specified...");
+        LOGGER.info("Starting Gradle Automatic Builder runPit() test when a test class is specified...");
 
-        InputConfiguration.get().setDescartesMode(false);
-
-        CtClass<Object> testClass = Utils.getInputConfiguration().getFactory().Class().get("example.TestSuiteExample");
-
-        sut.runPit(testClass);
+        sut.runPit(launcher.getFactory().Class().get("example.TestSuiteExample"));
         List<? extends AbstractPitResult> pitResults = parser.parseAndDelete("src/test/resources/test-projects/"+ sut.getOutputDirectoryPit());
 
         assertTrue("PIT results shouldn't be null", pitResults != null);
         assertTrue("PIT results shouldn't be empty", !pitResults.isEmpty());
 
-        Utils.LOGGER.info("Gradle Automatic Builder runPit() test complete when a test class is specified.");
+        LOGGER.info("Gradle Automatic Builder runPit() test complete when a test class is specified.");
     }
 
     private void cleanTestEnv() throws IOException {
         File classPathFile = new File("src/test/resources/test-projects/gjp_cp");
         if (classPathFile.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting java classpath file...");
+            LOGGER.debug("Cleaning Test Env - Deleting java classpath file...");
             classPathFile.delete();
         }
 
         File buildDir = new File("src/test/resources/test-projects/build");
         if (buildDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project build dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project build dir...");
             FileUtils.deleteDirectory(buildDir);
         }
 
         File gradlewDir = new File("src/test/resources/test-projects/gradle");
         if (gradlewDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle dir...");
             FileUtils.deleteDirectory(gradlewDir);
         }
 
         File gradleHiddenDir = new File("src/test/resources/test-projects/.gradle");
         if (gradleHiddenDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle hidden dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle hidden dir...");
             FileUtils.deleteDirectory(gradleHiddenDir);
         }
 
-        Utils.LOGGER.debug("Test Env cleaning complete.");
+        LOGGER.debug("Test Env cleaning complete.");
     }
 
 }
--- dspot/src/test/java/eu/stamp_project/automaticbuilder/gradle/GradleAutomaticBuilderWithDescartesTest.java
+++ dspot/src/test/java/eu/stamp_project/automaticbuilder/gradle/GradleAutomaticBuilderWithDescartesTest.java
@@ -1,8 +1,8 @@
 package eu.stamp_project.automaticbuilder.gradle;
 
-import eu.stamp_project.Utils;
+import eu.stamp_project.Main;
 import eu.stamp_project.automaticbuilder.AutomaticBuilder;
-import eu.stamp_project.automaticbuilder.AutomaticBuilderFactory;
+import eu.stamp_project.utils.options.AutomaticBuilderEnum;
 import eu.stamp_project.utils.pit.AbstractPitResult;
 import eu.stamp_project.utils.pit.PitXMLResultParser;
 import eu.stamp_project.utils.program.InputConfiguration;
@@ -10,6 +10,9 @@ import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
 
 import java.io.File;
@@ -25,75 +28,82 @@ import static org.junit.Assert.assertTrue;
  */
 public class GradleAutomaticBuilderWithDescartesTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GradleAutomaticBuilderTest.class);
+
     private AutomaticBuilder sut = null;
-    PitXMLResultParser parser;
+
+    private PitXMLResultParser parser;
+
+    private final String pathToProjectRoot = "src/test/resources/test-projects/";
+
+    private Launcher launcher;
 
     @Before
     public void setUp() throws Exception {
-
+        Main.verbose = true;
         cleanTestEnv();
-
-        Utils.init("src/test/resources/test-projects/test-projects.properties");
-
-        Utils.LOGGER.debug("Test Set-up - Reading input parameters...");
-        InputConfiguration inputConfiguration = Utils.getInputConfiguration();
-        inputConfiguration.setBuilderName("GradleBuilder");
-        inputConfiguration.setDescartesMutators("1");
-
-        Utils.LOGGER.debug("Test Set-up - instantiating Automatic Builder (SUT)...");
-        sut = AutomaticBuilderFactory.getAutomaticBuilder(inputConfiguration.getBuilderName());
-        Utils.LOGGER.debug("Test Set-up complete.");
+        LOGGER.debug("Test Set-up - Reading input parameters...");
+        LOGGER.debug("Test Set-up - instantiating Automatic Builder (SUT)...");
+        final InputConfiguration configuration = new InputConfiguration();
+        configuration.setAbsolutePathToProjectRoot(this.pathToProjectRoot);
+        configuration.setDescartesMode(false);
+        configuration.setFilter("example.*");
+        launcher = new Launcher();
+        launcher.addInputResource(pathToProjectRoot + "src/");
+        launcher.getEnvironment().setNoClasspath(true);
+        launcher.buildModel();
+        sut = AutomaticBuilderEnum.Gradle.getAutomaticBuilder(configuration);
+        sut.compile();
+        LOGGER.debug("Test Set-up complete.");
         parser = new PitXMLResultParser();
+
     }
 
     @After
     public void tearDown() throws Exception {
-        Utils.LOGGER.debug("Test Tear-down...");
-
+        LOGGER.debug("Test Tear-down...");
         cleanTestEnv();
-
-        Utils.LOGGER.debug("Test Tear-down complete.");
+        LOGGER.debug("Test Tear-down complete.");
     }
 
 
     @Test
     public void runPit_whenAllDescartesMutatorsAreSpecified() throws Exception {
-        Utils.LOGGER.info("Starting Gradle Automatic Builder runPit() test when a test class is specified...");
-        CtClass<Object> testClass = Utils.getInputConfiguration().getFactory().Class().get("example.TestSuiteExample");
-        InputConfiguration.get().setDescartesMode(true);
+        LOGGER.info("Starting Gradle Automatic Builder runPit() test when a test class is specified...");
+        CtClass<Object> testClass = launcher.getFactory().Class().get("example.TestSuiteExample");
         sut.runPit( testClass);
         List<? extends AbstractPitResult> pitResults = parser.parseAndDelete("src/test/resources/test-projects/" + sut.getOutputDirectoryPit());
         assertTrue("PIT results shouldn't be null", pitResults != null);
         assertTrue("PIT results shouldn't be empty", !pitResults.isEmpty());
-        Utils.LOGGER.info("Gradle Automatic Builder runPit() test complete when a test class is specified.");
+        LOGGER.info("Gradle Automatic Builder runPit() test complete when a test class is specified.");
     }
 
     private void cleanTestEnv() throws IOException {
         File classPathFile = new File("src/test/resources/test-projects/gjp_cp");
         if (classPathFile.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting java classpath file...");
+            LOGGER.debug("Cleaning Test Env - Deleting java classpath file...");
             classPathFile.delete();
         }
 
         File buildDir = new File("src/test/resources/test-projects/build");
         if (buildDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project build dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project build dir...");
             FileUtils.deleteDirectory(buildDir);
         }
 
         File gradlewDir = new File("src/test/resources/test-projects/gradle");
         if (gradlewDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle dir...");
             FileUtils.deleteDirectory(gradlewDir);
         }
 
         File gradleHiddenDir = new File("src/test/resources/test-projects/.gradle");
         if (gradleHiddenDir.exists()) {
-            Utils.LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle hidden dir...");
+            LOGGER.debug("Cleaning Test Env - Deleting Gradle Java project gradle hidden dir...");
             FileUtils.deleteDirectory(gradleHiddenDir);
         }
 
-        Utils.LOGGER.debug("Test Env cleaning complete.");
+        LOGGER.debug("Test Env cleaning complete.");
     }
 
 }
