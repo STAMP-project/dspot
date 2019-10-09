e9f130e07698ef98461e2819d08dfbb2300e3112
danglotb <benjamin.danglot@inria.fr>
2019-10-09 11:47:34 +0200

    ci: fix the command line to run dspot on dhell

--- .travis/travis-dhell.sh
+++ .travis/travis-dhell.sh
@@ -5,7 +5,7 @@ DPSOT_VERSION=${1}
 cd dspot
 git clone https://github.com/STAMP-project/dhell.git
 
-java -jar ../target/dspot-${DSPOT_VERSION}-jar-with-dependencies.jar --absolute-path-to-project-root dhell/ --iteration 1 --amplifiers MethodAdd --test eu.stamp_project.examples.dhell.HelloAppTest
+java -jar target/dspot-${DSPOT_VERSION}-jar-with-dependencies.jar --absolute-path-to-project-root dhell/ --iteration 1 --amplifiers MethodAdd --test eu.stamp_project.examples.dhell.HelloAppTest
 
 if [ -f dspot-out/eu/stamp_project/examples/dhell/HelloAppTest.java ]; then
     exit 0
