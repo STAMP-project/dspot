31451d04ae8c9d3adac41e7b3041c046c722bda9
danglotb <benjamin.danglot@inria.fr>
2019-10-09 12:07:30 +0200

    ci: update amplification of dhell command line options

--- .travis/travis-dhell.sh
+++ .travis/travis-dhell.sh
@@ -5,9 +5,9 @@ DPSOT_VERSION=${1}
 cd dspot
 git clone https://github.com/STAMP-project/dhell.git
 
-java -jar target/dspot-${DSPOT_VERSION}-jar-with-dependencies.jar --absolute-path-to-project-root dhell/ --iteration 1 --amplifiers MethodAdd --test eu.stamp_project.examples.dhell.HelloAppTest
+java -jar target/dspot-${DSPOT_VERSION}-jar-with-dependencies.jar --absolute-path-to-project-root dhell/ --iteration 1 --amplifiers MethodDuplicationAmplifier --test eu.stamp_project.examples.dhell.HelloAppTest
 
-if [ -f dspot-out/eu/stamp_project/examples/dhell/HelloAppTest.java ]; then
+if [ -f target/dspot/output/eu/stamp_project/examples/dhell/HelloAppTest.java ]; then
     exit 0
 else
     exit 1
