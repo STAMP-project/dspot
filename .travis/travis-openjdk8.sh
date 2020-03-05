df9910b4a2fb7771bdd43b90b1754f4c7edd99e4
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-05 19:59:33 +0100

    @monperrus at work

--- .travis/travis-openjdk8.sh
+++ .travis/travis-openjdk8.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-source /opt/jdk_switcher/jdk_switcher.sh
-
-jdk_switcher use openjdk8
-
 # see doIntegrationTests=true -> see https://stackoverflow.com/a/15881238
-mvn -Djava.src.version=1.8 test -DdoIntegrationTests=true -f dspot/pom.xml
+mvn -Djava.src.version=1.8 -DdoIntegrationTests=true test -f dspot/pom.xml
