317458076f944962fa9ec76c041a3cd25749e7bf
Carolin Brandt <c.e.brandt@tudelft.nl>
2020-06-05 06:19:10 +0200

    less lines output when installing python packages

--- .travis/travis-prettifier.sh
+++ .travis/travis-prettifier.sh
@@ -4,6 +4,6 @@ source /opt/jdk_switcher/jdk_switcher.sh
 
 ./dspot-prettifer/src/test/bash/install_code2vec.sh
 
-pip3 install bottleneck numpy keras tensorflow
+pip3 install --quiet bottleneck numpy keras tensorflow
 
 jdk_switcher use openjdk8 & mvn -Djava.src.version=1.8 test -f dspot-prettifier/pom.xml
