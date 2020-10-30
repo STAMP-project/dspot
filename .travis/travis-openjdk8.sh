56d55cfe010161e2c65e330a31de01bd18a80d31
Martin Monperrus <martin.monperrus@gnieh.org>
2020-10-30 06:48:00 +0100

    up

--- .travis/travis-openjdk8.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# see doIntegrationTests=true -> see https://stackoverflow.com/a/15881238
-mvn -Djava.src.version=1.8 -DdoIntegrationTests=true test -f dspot/pom.xml
