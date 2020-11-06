e4fad2f5bf57f17e6e675de208ac3bfda62e9078
Martin Monperrus <martin.monperrus@gnieh.org>
2020-11-06 23:02:48 +0100

    @monperrus at work

--- .travis/travis-coverage.sh
+++ .travis/travis-coverage.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-cd dspot 
-
-mvn test -D test=eu.stamp_project.dspot.selector.ChangeDetectorSelectorTest
-
-mvn -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID -DdoIntegrationTests=true clean test jacoco:report coveralls:report
+cd dspot && mvn -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID -DdoIntegrationTests=true clean test jacoco:report coveralls:report
