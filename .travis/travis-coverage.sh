aa3bb0c10fec1722bc825d3d48abf4a3e4bd297a
Martin Monperrus <martin.monperrus@gnieh.org>
2020-11-08 09:53:09 +0100

    test: change default logging level in CI

--- .travis/travis-coverage.sh
+++ .travis/travis-coverage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd dspot && mvn -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID -DdoIntegrationTests=true clean test
+cd dspot && mvn -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID -DdoIntegrationTests=true -Dorg.slf4j.simpleLogger.defaultLogLevel=error clean test
