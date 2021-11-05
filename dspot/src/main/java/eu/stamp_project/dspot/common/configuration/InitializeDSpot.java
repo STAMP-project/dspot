9352e548fba03802df4e1ff2bf26bbda5599b359
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-11-05 15:00:42 +0100

    remove commented out code

--- dspot/src/main/java/eu/stamp_project/dspot/common/configuration/InitializeDSpot.java
+++ dspot/src/main/java/eu/stamp_project/dspot/common/configuration/InitializeDSpot.java
@@ -148,16 +148,6 @@ public class InitializeDSpot {
             dependencies = automaticBuilder.compileAndBuildClasspath();
             configuration.setDependencies(dependencies);
         }
-        // TODO checks this. Since we support different Test Support, we may not need to add artificially junit in the classpath
-//        if (!dependencies.contains("junit" + File.separator + "junit" + File.separator + "4")) {
-//            dependencies = Test.class
-//                    .getProtectionDomain()
-//                    .getCodeSource()
-//                    .getLocation()
-//                    .getFile() +
-//                    AmplificationHelper.PATH_SEPARATOR + dependencies;
-//            System.out.println("dependencies at end of junit block: " + dependencies);
-//        }
         if (!additionalClasspathElements.isEmpty()) {
             String pathToAdditionalClasspathElements = additionalClasspathElements;
             if (!Paths.get(additionalClasspathElements).isAbsolute()) {
